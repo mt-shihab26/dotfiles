8249274ccc3179034e7adb8921cc38a69a3b34f4
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-01 03:37:02 +0600

    move stylua to root

--- .config/wezterm/background.lua
+++ .config/wezterm/background.lua
@@ -1,16 +1,16 @@
-local HOME = os.getenv("HOME")
+local HOME = os.getenv "HOME"
 
 local file = HOME .. "/.assets/wallpapers/hacking.jpg"
 
 -- file = "/Users/shihab/.assets/wallpapers/hacking.jpg"
 
 local background = {
-	{
-		source = { File = file },
-		hsb = { hue = 1.0, saturation = 1.02, brightness = 0.04 },
-		width = "100%",
-		height = "100%",
-	},
+    {
+        source = { File = file },
+        hsb = { hue = 1.0, saturation = 1.02, brightness = 0.04 },
+        width = "100%",
+        height = "100%",
+    },
 }
 
 return background
