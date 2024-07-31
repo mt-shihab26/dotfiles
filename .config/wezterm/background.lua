9a2b2c38dcdf692690431c150ec73913eea9b3fe
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-01 03:44:53 +0600

    update

--- .config/wezterm/background.lua
+++ .config/wezterm/background.lua
@@ -4,13 +4,11 @@ local file = HOME .. "/.assets/wallpapers/hacking.jpg"
 
 -- file = "/Users/shihab/.assets/wallpapers/hacking.jpg"
 
-local background = {
+return {
     {
         source = { File = file },
-        hsb = { hue = 1.0, saturation = 1.02, brightness = 0.04 },
+        hsb = { hue = 1.0, saturation = 1.02, brightness = 0.03 },
         width = "100%",
         height = "100%",
     },
 }
-
-return background
