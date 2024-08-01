25ccb78aa63f62334221521b276cc035307e77b2
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-01 13:14:06 +0600

    update

--- .config/wezterm/background.lua
+++ .config/wezterm/background.lua
@@ -1,11 +1,11 @@
 local HOME = os.getenv "HOME"
 
-local file = HOME .. "/.assets/wallpapers/go.jpg"
+local file = HOME .. "/.assets/wallpapers/hacking.jpg"
 
 return {
     {
         source = { File = file },
-        hsb = { hue = 1.0, saturation = 1.02, brightness = 0.03 },
+        hsb = { hue = 1.0, saturation = 1.02, brightness = 0.02 },
         width = "100%",
         height = "100%",
     },
