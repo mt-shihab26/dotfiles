942aa8cb24f8825281d7671a764525e933ea5889
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-04 01:21:14 +0600

    blade syntax highlighting setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,26 +43,6 @@ require("lazy").setup {
         {
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
-            config = function()
-                local configs = require "nvim-treesitter.configs"
-                configs.setup {
-                    ensure_installed = {
-                        "html",
-                        "css",
-                        "markdown",
-                        "markdown_inline",
-                        "c",
-                        "lua",
-                        "elixir",
-                        "javascript",
-                        "typescript",
-                        "php",
-                    },
-                    sync_install = false,
-                    highlight = { enable = true },
-                    indent = { enable = true },
-                }
-            end,
         },
         {
             "stevearc/conform.nvim",
@@ -100,3 +80,40 @@ require("conform").setup {
         blade = { "blade-formatter" },
     },
 }
+
+-- treesitter config
+local configs = require "nvim-treesitter.configs"
+configs.setup {
+    ensure_installed = {
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "c",
+        "lua",
+        "elixir",
+        "javascript",
+        "typescript",
+        "php",
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+}
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.blade = {
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+    filetype = "blade",
+}
+
+vim.filetype.add {
+    pattern = {
+        [".*%.blade%.php"] = "blade",
+    },
+}
+
