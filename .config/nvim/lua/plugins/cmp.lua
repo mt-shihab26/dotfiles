c7cb04744720e97b52cebd29de6632f1a8ab10dc
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-05 23:54:59 +0600

    cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,15 +1,17 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local cmp = require "cmp"
+            cmp.setup {
+                sources = {
+                    { name = "nvim_lsp" },
+                },
+            }
+        end,
     },
-    config = function()
-        local cmp = require "cmp"
-        cmp.setup {
-            sources = {
-                { name = "nvim_lsp" },
-            },
-        }
-    end,
 }
