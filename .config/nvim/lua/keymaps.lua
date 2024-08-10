fd9e8f7d451f7c90822efa0f15be2cf85aeb7ddb
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-11 02:05:39 +0600

    fixing lsp setting with nvim whick-key

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,68 +1,51 @@
-local map = vim.keymap.set
-local wk = require "which-key"
+require("which-key").add {
+    { "jk", "<ESC>", mode = "i", desc = "exit insert mode" },
 
-local opts = function(options)
-    local default_options = { noremap = true, silent = true }
-    return vim.tbl_extend("force", default_options, options or {})
-end
+    -- comment
+    { "<leader>/", "gcc", mode = "n", desc = "comment toggle" },
+    { "<leader>/", "gc", mode = "v", desc = "comment toggle" },
 
-map("i", "jk", "<ESC>", opts { desc = "exit insert mode" })
+    -- tree
+    { "<leader>e", "<CMD>NvimTreeFocus<CR>", mode = "n", desc = "open file tree" },
+    { "<leader>p", "<CMD>NvimTreeClose<CR>", mode = "n", desc = "close file tree" },
+    { "<C-n>", "<CMD>NvimTreeToggle<CR>", mode = "n", desc = "toggle file tree" },
 
--- Comment
+    -- tabs
+    { "<tab>", "<CMD>BufferLineCycleNext<CR>", mode = "n", desc = "next buffer" },
+    { "<S-tab>", "<CMD>BufferLineCyclePrev<CR>", mode = "n", desc = "previous buffer" },
+    { "<leader>c", "<CMD>bp<bar>sp<bar>bn<bar>bd<CR>", mode = "n", desc = "close buffer" },
 
-map("n", "<leader>/", "gcc", opts { desc = "comment toggle", remap = true })
-map("v", "<leader>/", "gc", opts { desc = "comment toggle", remap = true })
+    -- terminal
+    { "<leader>t", "<CMD>ToggleTerm<CR>", mode = "n", desc = "open terminal" },
 
--- LSP Config
-map("n", "K", vim.lsp.buf.hover, opts { desc = "show hover information" })
-map("n", "L", vim.diagnostic.open_float, opts { desc = "show hover line diagnostics" })
-map("n", "<leader>k", vim.lsp.buf.signature_help, opts { desc = "show signature help" })
-map("n", "<leader>D", vim.lsp.buf.type_definition, opts { desc = "show type definition" })
-map("n", "gd", vim.lsp.buf.definition, opts { desc = "go to definition" })
-map("n", "gD", vim.lsp.buf.declaration, opts { desc = "go to declaration" })
-map("n", "gi", vim.lsp.buf.implementation, opts { desc = "go to implementation" })
-map("n", "gr", vim.lsp.buf.references, opts { desc = "list all references" })
-map("n", "<leader>r", vim.lsp.buf.rename, opts { desc = "rename symbol" })
-map({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts { desc = "show code action" })
+    -- copy
+    { "<leader>y", "<CMD>CopyFilePath<CR>", mode = "n", desc = "copy file path" },
 
--- Tree
-wk.add {
-    { "<leader>e", "<cmd>NvimTreeFocus<CR>", mode = "n", desc = "open file tree" },
-    { "<leader>p", "<cmd>NvimTreeClose<CR>", mode = "n", desc = "close file tree" },
-    { "<C-n>", "<cmd>NvimTreeToggle<CR>", mode = "n", desc = "toggle file tree" },
-}
+    -- git
+    { "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", mode = "n", desc = "preview git line change" },
 
--- Telescope
-wk.add {
+    -- lsp
+    { "K", vim.lsp.buf.hover, mode = "n", desc = "show hover information" },
+    { "L", vim.diagnostic.open_float, mode = "n", desc = "show hover line diagnostics" },
+    { "gd", vim.lsp.buf.definition, mode = "n", desc = "go to definition" },
+    { "gD", vim.lsp.buf.declaration, mode = "n", desc = "go to declaration" },
+    { "<leader>a", vim.lsp.buf.code_action, mode = { "n", "v" }, desc = "show code action" },
+    { "<leader>r", vim.lsp.buf.rename, mode = "n", desc = "rename symbol" },
+    { "<leader>D", vim.lsp.buf.type_definition, mode = "n", desc = "show type definition" },
+    { "gi", vim.lsp.buf.implementation, mode = "n", desc = "go to implementation" },
+    { "gr", vim.lsp.buf.references, mode = "n", desc = "list all references" },
+
+    -- telescope
     { "<leader>f", group = "telescope" },
-    { "<leader>ff", "<cmd>TelescopeProjectFiles<CR>", mode = "n", desc = "find file" },
-    { "<leader>fw", "<cmd>Telescope live_grep<CR>", mode = "n", desc = "live grep" },
-    { "<leader>fd", "<cmd>TelescopeDotFiles<CR>", mode = "n", desc = "find dotfiles" },
+    { "<leader>ff", "<CMD>TelescopeProjectFiles<CR>", mode = "n", desc = "find file" },
+    { "<leader>fw", "<CMD>Telescope live_grep<CR>", mode = "n", desc = "live grep" },
+    { "<leader>fd", "<CMD>TelescopeDotFiles<CR>", mode = "n", desc = "find dotfiles" },
+
+    -- integrations
+    { "<leader>o", group = "open things" },
+    { "<leader>og", "<CMD>LazyGit<CR>", mode = "n", desc = "open lazygit" },
+    { "<leader>od", "<CMD>LazyDocker<CR>", mode = "n", desc = "open lazydocker" },
+    { "<leader>os", "<CMD>LazySQL<CR>", mode = "n", desc = "open lazysql" },
+    { "<leader>ol", "<CMD>OpenInBrowser<CR>", mode = { "n", "v" }, desc = "open in browser" },
+    { "<leader>ot", "<CMD>Themery<CR>", mode = "n", desc = "open colorscheme changer" },
 }
-
--- Tabs
-wk.add {
-    { mode = "n", "<tab>", "<cmd>BufferLineCycleNext<CR>", desc = "next buffer" },
-    { mode = "n", "<S-tab>", "<cmd>BufferLineCyclePrev<CR>", desc = "previous buffer" },
-    { mode = "n", "<leader>c", "<cmd>bp<bar>sp<bar>bn<bar>bd<CR>", desc = "close buffer" },
-}
-
--- Colorscheme
-map("n", "<leader>tc", "<cmd>Themery<CR>", opts { desc = "open colorscheme changer" })
-
--- Integrations
-map("n", "<leader>lg", "<cmd>LazyGit<CR>", opts { desc = "open lazygit" })
-map("n", "<leader>ld", "<cmd>LazyDocker<CR>", opts { desc = "open lazydocker" })
-map("n", "<leader>ls", "<cmd>LazySQL<CR>", opts { desc = "open lazysql" })
-
--- Terminal
-map("n", "<leader>t", "<cmd>ToggleTerm<CR>", opts { desc = "open terminal" })
-
--- Copy
-map("n", "<leader>y", "<cmd>CopyFilePath<CR>", opts { desc = "copy file path" })
-
--- Git
-map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "preview git line change" })
-
--- Link open
-map({ "n", "v" }, "<leader>ol", "<cmd>OpenInBrowser<CR>", opts { desc = "open in browser" })
