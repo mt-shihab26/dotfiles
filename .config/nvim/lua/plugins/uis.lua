50d1e677a2210352863bab69cb06d3afa6fbaca8
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-13 04:29:23 +0600

    dropbar added

--- .config/nvim/lua/plugins/uis.lua
+++ .config/nvim/lua/plugins/uis.lua
@@ -156,4 +156,13 @@ return {
             }
         end,
     },
+    {
+        "Bekaboo/dropbar.nvim",
+        dependencies = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+        },
+        config = function()
+            vim.ui.select = require("dropbar.utils.menu").select
+        end,
+    },
 }
