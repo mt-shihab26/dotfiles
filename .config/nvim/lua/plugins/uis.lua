54cc401f2978b3c70363765798ba3ab45d70f7a5
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-13 21:32:32 +0600

    update

--- .config/nvim/lua/plugins/uis.lua
+++ .config/nvim/lua/plugins/uis.lua
@@ -156,13 +156,13 @@ return {
             }
         end,
     },
-    {
-        "Bekaboo/dropbar.nvim",
-        dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-        },
-        config = function()
-            vim.ui.select = require("dropbar.utils.menu").select
-        end,
-    },
+    -- {
+    --     "Bekaboo/dropbar.nvim",
+    --     dependencies = {
+    --         "nvim-telescope/telescope-fzf-native.nvim",
+    --     },
+    --     config = function()
+    --         vim.ui.select = require("dropbar.utils.menu").select
+    --     end,
+    -- },
 }
