1d2ba34c9e113b07e776915c8b836bccbde3d95d
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-17 03:44:25 +0600

    update

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -60,4 +60,47 @@ return {
             }
         end,
     },
+    {
+        "nvimdev/dashboard-nvim",
+        opts = {
+            theme = "hyper",
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    {
+                        action = "Lazy",
+                        desc = " Lazy",
+                        icon = "󰒲 ",
+                        key = "l",
+                        group = "Label",
+                    },
+                    {
+                        action = "Mason",
+                        desc = "Mason",
+                        icon = " ",
+                        key = "m",
+                        group = "@property",
+                    },
+                    {
+                        action = "lua LazyVim.pick.config_files()()",
+                        desc = " Config",
+                        icon = " ",
+                        key = "c",
+                        group = "@property",
+                    },
+                    {
+                        action = function()
+                            vim.api.nvim_input "<cmd>qa<cr>"
+                        end,
+                        desc = " Quit",
+                        icon = " ",
+                        key = "q",
+                        group = "Label",
+                    },
+                },
+            },
+        },
+    },
 }
