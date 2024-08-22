474af245d0865bef6c4f40fe6892c4d385f916dc
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-23 03:05:37 +0600

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font "MesloLGS Nerd Font Mono"
-config.font_size = 17
+config.font_size = 16
 
 config.enable_tab_bar = false
 
