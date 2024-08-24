4210de2733630e5aedfc1b7ee8fa068615e3e29c
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-08-24 06:01:21 +0600

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font "JetBrains Mono"
-config.font_size = 16
+config.font_size = 15
 
 config.enable_tab_bar = false
 
