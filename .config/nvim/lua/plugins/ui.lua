0967ca27d7421e64d2c6cdf516b16f005c5e5714
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-09-13 13:17:29 +0600

    config plugins"

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,147 @@
+return {
+    -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+    -- NOTE: Plugins can also be added by using a table,
+    -- with the first argument being the link and the following
+    -- keys can be used to configure plugin behavior/loading/etc.
+    --
+    -- Use `opts = {}` to force a plugin to be loaded.
+    --
+
+    -- Here is a more advanced example where we pass configuration
+    -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+    --    require('gitsigns').setup({ ... })
+    --
+    -- See `:help gitsigns` to understand what the configuration keys do
+    -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    --     "lewis6991/gitsigns.nvim",
+    --     opts = {
+    --         signs = {
+    --             add = { text = "+" },
+    --             change = { text = "~" },
+    --             delete = { text = "_" },
+    --             topdelete = { text = "‾" },
+    --             changedelete = { text = "~" },
+    --         },
+    --     },
+    -- },
+
+    -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+    --
+    -- This is often very useful to both group configuration, as well as handle
+    -- lazy loading plugins that don't need to be loaded immediately at startup.
+    --
+    -- For example, in the following configuration, we use:
+    --  event = 'VimEnter'
+    --
+    -- which loads which-key before all the UI elements are loaded. Events can be
+    -- normal autocommands events (`:help autocmd-events`).
+    --
+    -- Then, because we use the `config` key, the configuration only runs
+    -- after the plugin has been loaded:
+    --  config = function() ... end
+
+    { -- Useful plugin to show you pending keybinds.
+        "folke/which-key.nvim",
+        event = "VimEnter", -- Sets the loading event to 'VimEnter'
+        opts = {
+            icons = {
+                -- set icon mappings to true if you have a Nerd Font
+                mappings = vim.g.have_nerd_font,
+                -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+                -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+                keys = vim.g.have_nerd_font and {} or {
+                    Up = "<Up> ",
+                    Down = "<Down> ",
+                    Left = "<Left> ",
+                    Right = "<Right> ",
+                    C = "<C-…> ",
+                    M = "<M-…> ",
+                    D = "<D-…> ",
+                    S = "<S-…> ",
+                    CR = "<CR> ",
+                    Esc = "<Esc> ",
+                    ScrollWheelDown = "<ScrollWheelDown> ",
+                    ScrollWheelUp = "<ScrollWheelUp> ",
+                    NL = "<NL> ",
+                    BS = "<BS> ",
+                    Space = "<Space> ",
+                    Tab = "<Tab> ",
+                    F1 = "<F1>",
+                    F2 = "<F2>",
+                    F3 = "<F3>",
+                    F4 = "<F4>",
+                    F5 = "<F5>",
+                    F6 = "<F6>",
+                    F7 = "<F7>",
+                    F8 = "<F8>",
+                    F9 = "<F9>",
+                    F10 = "<F10>",
+                    F11 = "<F11>",
+                    F12 = "<F12>",
+                },
+            },
+
+            -- Document existing key chains
+            spec = {
+                { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+                { "<leader>d", group = "[D]ocument" },
+                { "<leader>r", group = "[R]ename" },
+                { "<leader>s", group = "[S]earch" },
+                { "<leader>w", group = "[W]orkspace" },
+                { "<leader>t", group = "[T]oggle" },
+                { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+            },
+        },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        version = "*",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        cmd = "Neotree",
+        keys = {
+            { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+            -- { "<leader>e", "<CMD>Neotree action=focus<CR>", desc = "Focus File Tree", remap = true },
+            -- { "<leader>p", "<CMD>Neotree action=close<CR>", desc = "Close File Tree", remap = true },
+            -- { "<C-n>", "<CMD>Neotree toggle<CR>", desc = "Toggle File Tree", remap = true },
+        },
+        opts = {
+            filesystem = {
+                window = {
+                    mappings = {
+                        ["\\"] = "close_window",
+                    },
+                },
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+            },
+        },
+    },
+    { -- Add indentation guides even on blank lines
+        "lukas-reineke/indent-blankline.nvim",
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = "ibl",
+        opts = {},
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        -- Optional dependency
+        dependencies = { "hrsh7th/nvim-cmp" },
+        config = function()
+            require("nvim-autopairs").setup {}
+            -- If you want to automatically add `(` after selecting a function or method
+            local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+            local cmp = require "cmp"
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
+}
