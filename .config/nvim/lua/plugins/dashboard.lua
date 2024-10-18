203e1be79a793b6f2b03ac8593fe25ca31478ce8
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-10-18 12:39:26 +0600

    bufferline

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,51 +1,48 @@
 return {
     "nvimdev/dashboard-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     config = function()
         require("dashboard").setup {
-            -- config
+            theme = "hyper",
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    {
+                        action = "Lazy",
+                        desc = " Lazy",
+                        icon = "󰒲 ",
+                        key = "l",
+                        group = "Label",
+                    },
+                    {
+                        action = "Mason",
+                        desc = "Mason",
+                        icon = " ",
+                        key = "m",
+                        group = "@property",
+                    },
+                    {
+                        action = "lua LazyVim.pick.config_files()()",
+                        desc = " Config",
+                        icon = " ",
+                        key = "c",
+                        group = "@property",
+                    },
+                    {
+                        action = function()
+                            vim.api.nvim_input "<cmd>qa<cr>"
+                        end,
+                        desc = " Quit",
+                        icon = " ",
+                        key = "q",
+                        group = "Label",
+                    },
+                },
+            },
         }
     end,
-    dependencies = {
-        { "nvim-tree/nvim-web-devicons" },
-    },
-    opts = {
-        theme = "hyper",
-        config = {
-            week_header = {
-                enable = true,
-            },
-            shortcut = {
-                {
-                    action = "Lazy",
-                    desc = " Lazy",
-                    icon = "󰒲 ",
-                    key = "l",
-                    group = "Label",
-                },
-                {
-                    action = "Mason",
-                    desc = "Mason",
-                    icon = " ",
-                    key = "m",
-                    group = "@property",
-                },
-                {
-                    action = "lua LazyVim.pick.config_files()()",
-                    desc = " Config",
-                    icon = " ",
-                    key = "c",
-                    group = "@property",
-                },
-                {
-                    action = function()
-                        vim.api.nvim_input "<cmd>qa<cr>"
-                    end,
-                    desc = " Quit",
-                    icon = " ",
-                    key = "q",
-                    group = "Label",
-                },
-            },
-        },
-    },
 }
