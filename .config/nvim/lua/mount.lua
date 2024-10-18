3f7fbd31f2878bf7c7ab12f4f91116bb7bf1fb1f
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-10-18 11:26:53 +0600

    neo tree fixed

--- .config/nvim/lua/mount.lua
+++ .config/nvim/lua/mount.lua
@@ -31,3 +31,12 @@ vim.opt.termguicolors = true
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- File type detection for .env and .env.example files
+vim.cmd [[
+  augroup dotenv
+    autocmd!
+    autocmd BufNewFile,BufRead .env,.env.* setfiletype sh
+    autocmd BufNewFile,BufRead .env.example setfiletype sh
+  augroup END
+]]
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -13,10 +14,34 @@ return {
         { "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Toggle File Tree", remap = true },
     },
     opts = {
+        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
         filesystem = {
             filtered_items = {
                 hide_dotfiles = false,
                 hide_gitignored = false,
+            },
+            follow_current_file = {
+                enabled = true, -- This will find and focus the file in the active buffer every time
+                leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            },
+        },
+        window = {
+            position = "left",
+            width = 40,
+            mapping_options = {
+                noremap = true,
+                nowait = true,
+            },
+        },
+        event_handlers = {
+            {
+                event = "before_render",
+                handler = function(_)
+                    vim.cmd "noautocmd"
+                end,
             },
         },
     },
