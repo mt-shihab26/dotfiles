06505b9a0db5d087aca2e57b313552b2ef300ab3
Shihab Mahamud <shihab4t@gmail.com>
2024-10-21 04:48:45 +0600

    tmux commands

--- .scripts/tmux-kill-session.sh
+++ .scripts/tmux-kill-session.sh
@@ -4,7 +4,7 @@
 tmux_running=$(pgrep tmux)
 
 if [[ -z $tmux_running ]]; then
-    echo "Tmux is not running. No sessions to kill."
+    echo "tmux is not running. no sessions to kill."
     exit 0
 fi
 
@@ -12,18 +12,18 @@ fi
 sessions=$(tmux list-sessions -F "#S")
 
 if [[ -z $sessions ]]; then
-    echo "No active tmux sessions found."
+    echo "no active tmux sessions found."
     exit 0
 fi
 
 # Use fzf to select a session
-selected=$(echo "$sessions" | fzf --prompt="Select session to kill: " --header="Active Tmux Sessions")
+selected=$(echo "$sessions" | fzf)
 
 if [[ -z $selected ]]; then
-    echo "No session selected. Exiting."
+    echo "no session selected. exiting."
     exit 0
 fi
 
 # Kill the selected session
 tmux kill-session -t "$selected"
-echo "Killed session: $selected"
+echo "killed session: $selected"
