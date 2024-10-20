9648ad56c84d5503e8536fdc72d77dfd4a2fff49
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-10-20 20:16:04 +0600

    remove seperator from lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,10 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = {
+        options = {
+            section_separators = "",
+            component_separators = "",
+        },
         sections = {
             lualine_y = {
                 { "location", padding = { left = 0, right = 1 } },
