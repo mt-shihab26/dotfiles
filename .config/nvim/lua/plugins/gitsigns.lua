73f2e583a8ce597ec466623244e5666a325cfb29
Shihab Mahamud <shihab4t@gmail.com>
2024-10-28 04:55:42 +0600

    update

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,23 +2,20 @@ return {
     "lewis6991/gitsigns.nvim",
     lazy = false,
     opts = {
-        preview_config = {
-            border = { "", "", "", " " },
-        },
-        current_line_blame = true,
-        current_line_blame_opts = {
-            virt_text = true,
-            virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
-            ignore_whitespace = false,
-        },
         signs = {
-            add = { text = "│" },
-            change = { text = "│" },
-            delete = { text = "_" },
-            topdelete = { text = "‾" },
-            changedelete = { text = "┄" },
-            untracked = { text = "┊" },
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "" },
+            topdelete = { text = "" },
+            changedelete = { text = "▎" },
+            untracked = { text = "▎" },
+        },
+        signs_staged = {
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "" },
+            topdelete = { text = "" },
+            changedelete = { text = "▎" },
         },
     },
 }
