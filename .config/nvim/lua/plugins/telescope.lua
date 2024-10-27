73be081851e56f63c937fdc3bfe8cd14d86407c3
Shihab Mahamud <shihab4t@gmail.com>
2024-10-27 15:31:33 +0600

    telescope keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,14 @@
 
 return {
     "nvim-telescope/telescope.nvim",
+    opts = {
+        defaults = {
+            layout_strategy = "horizontal",
+            layout_config = { prompt_position = "top" },
+            sorting_strategy = "ascending",
+            winblend = 0,
+        },
+    },
     keys = {
         { "<leader>/", false },
         { "<leader>,", false },
