5e1f7afe908020a934f24abc9361cd45ce83f938
Shihab Mahamud <shihab4t@gmail.com>
2024-10-27 23:40:20 +0600

    dashboard

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,9 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     opts = {
         options = {
             mode = "buffers",
