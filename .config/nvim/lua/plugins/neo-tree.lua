eb5b779deb9ce3823001a9a4f5e87ec3e2629ce4
Shihab Mahamud <shihab4t@gmail.com>
2024-10-27 16:14:33 +0600

    bufferline

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,25 +2,25 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '<leader>e', '<cmd>Neotree action=focus<cr>', desc = 'focus file tree', remap = true },
-    { '<leader>p', '<cmd>Neotree action=close<cr>', desc = 'close file tree', remap = true },
-    { '<C-n>', '<cmd>Neotree toggle<cr>', desc = 'toggle file tree', remap = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
     },
-  },
+    cmd = "Neotree",
+    keys = {
+        { "<leader>e", "<cmd>Neotree action=focus<cr>", desc = "focus file tree", remap = true },
+        { "<leader>p", "<cmd>Neotree action=close<cr>", desc = "close file tree", remap = true },
+        { "<C-n>", "<cmd>Neotree toggle<cr>", desc = "toggle file tree", remap = true },
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+        },
+    },
 }
