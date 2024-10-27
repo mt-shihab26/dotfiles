c950adfa1700cb0b0e19ca82cf6b1c3ff869307c
Shihab Mahamud <shihab4t@gmail.com>
2024-10-27 22:22:56 +0600

    fix neo-tree follow current file

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -3,9 +3,9 @@ return {
     branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
-        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "3rd/image.nvim",
     },
     cmd = "Neotree",
     keys = {
@@ -19,6 +19,12 @@ return {
                 hide_dotfiles = false,
                 hide_gitignored = false,
             },
+            follow_current_file = {
+                enabled = true,
+                leave_dirs_open = true,
+            },
+            hijack_netrw_behavior = "open_current",
+            use_libuv_file_watcher = true,
         },
     },
 }
