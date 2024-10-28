856ba52bd514795fcc0911ac389771957c271401
Shihab Mahamud <shihab4t@gmail.com>
2024-10-29 02:48:54 +0600

    lsp-file-operations

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -162,6 +162,17 @@ return {
         local lspconfig = require "lspconfig"
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+        -- Set global defaults for all servers
+        lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+            capabilities = vim.tbl_deep_extend(
+                "force",
+                vim.lsp.protocol.make_client_capabilities(),
+                -- returns configured operations if setup() was already called
+                -- or default operations if not
+                require("lsp-file-operations").default_capabilities()
+            ),
+        })
+
         local on_attach = function(_, bufnr)
             local opts = function(opts)
                 return vim.tbl_extend("force", {
