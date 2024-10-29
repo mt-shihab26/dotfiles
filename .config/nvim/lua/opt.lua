d3fe0b4389fd208d12443e43ac5f1c59b8a6322b
Shihab Mahamud <shihab4t@gmail.com>
2024-10-29 16:45:37 +0600

    update

--- .config/nvim/lua/opt.lua
+++ .config/nvim/lua/opt.lua
@@ -4,7 +4,9 @@ vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.clipboard = "unnamedplus"
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
 
 vim.opt.mouse = "a"
 
@@ -19,9 +21,18 @@ vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = "→ ", trail = "·" }
+vim.opt.listchars = { tab = "→ ", trail = "·", nbsp = "␣" }
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
