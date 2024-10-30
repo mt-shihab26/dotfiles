482d896539841f2554048d09adfe7d6fcf71668c
Shihab Mahamud <shihab4t@gmail.com>
2024-10-31 00:28:22 +0600

    update

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,9 +1,13 @@
 -- file type detection for .env and .env.* files
-vim.cmd "augroup dotenv | autocmd! | autocmd BufNewFile,BufRead .env,.env.* setfiletype sh | augroup END"
+vim.cmd [[
+  augroup dotenv
+    autocmd!
+    autocmd BufNewFile,BufRead .env,.env.* setfiletype sh
+  augroup END
+]]
 
--- Highlight on yank
+-- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = augroup "highlight_yank",
     callback = function()
         vim.highlight.on_yank()
     end,
