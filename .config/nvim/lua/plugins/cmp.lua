793c429985c505de76210b3855f37155468b3b37
Shihab Mahamud <shihab4t@gmail.com>
2024-10-31 04:39:51 +0600

    clear up cmp config with custom vscode like snippit

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,3 +1,34 @@
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local tab_fallback = function(cmp, luasnip)
+    return function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+        elseif has_words_before() then
+            cmp.complete()
+        else
+            fallback()
+        end
+    end
+end
+
+local shift_tab_fallback = function(cmp, luasnip)
+    return function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end
+end
+
 return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -7,55 +38,19 @@ return {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-calc",
+        "hrsh7th/cmp-cmdline",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-        "onsails/lspkind-nvim",
+        "rafamadriz/friendly-snippets",
     },
     config = function()
+        ---@type table
         local cmp = require "cmp"
+        ---@type table
         local luasnip = require "luasnip"
 
-        require("luasnip/loaders/from_snipmate").lazy_load()
-
-        local has_words_before = function()
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-        end
-
         cmp.setup {
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "nvim_lsp_signature_help" },
-                { name = "buffer" },
-                { name = "path" },
-                { name = "calc" },
-                { name = "luasnip" },
-            },
-            mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
-                        luasnip.jump(1)
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<CR>"] = cmp.mapping.confirm {
-                    select = true,
-                },
-            },
+            preselect = cmp.PreselectMode.Item,
             completion = {
                 completeopt = "menu,menuone,noinsert",
             },
@@ -64,7 +59,30 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            preselect = cmp.PreselectMode.Item,
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = {
+                ["<CR>"] = cmp.mapping.confirm { select = true },
+                ["<Tab>"] = cmp.mapping(tab_fallback(cmp, luasnip), { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(shift_tab_fallback(cmp, luasnip), { "i", "s" }),
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "nvim_lsp_signature_help" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "calc" },
+                { name = "cmdline" },
+                { name = "luasnip" },
+            },
         }
+
+        local loader = require "luasnip.loaders.from_vscode"
+
+        -- Load snippets
+        loader.lazy_load() -- load friendly-snippets
+        loader.lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } } -- load ~/.config/nvim/snippets
     end,
 }
