d2d12beef6388414618a3237d4d4cffe28d371fd
Shihab Mahamud <shihab4t@gmail.com>
2024-11-01 22:12:57 +0600

    cmp fixed

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -41,28 +41,22 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
-        "onsails/lspkind-nvim",
     },
     config = function()
+        local sources = {
+            nvim_lsp = "[lsp]",
+            nvim_lsp_signature_help = "[signature]",
+            buffer = "[buffer]",
+            path = "[path]",
+            calc = "[calc]",
+            luasnip = "[snippet]",
+        }
+
         ---@type table
         local cmp = require "cmp"
         ---@type table
         local luasnip = require "luasnip"
         ---@type table
-        local lspkind = require "lspkind"
-
-        local source_map = {
-            nvim_lsp = "lsp",
-            nvim_lsp_signature_help = "signature",
-            buffer = "buffer",
-            path = "path",
-            calc = "calc",
-            luasnip = "luasnip",
-        }
-
-        local function ltrim(s)
-            return s:match "^%s*(.*)"
-        end
 
         cmp.setup {
             preselect = cmp.PreselectMode.Item,
@@ -93,39 +87,11 @@ return {
                 { name = "luasnip" },
             },
             formatting = {
-                fields = { "kind", "abbr", "menu" },
-                format = lspkind.cmp_format {
-                    mode = "symbol",
-                    before = function(entry, vim_item)
-                        -- Replace the 'menu' field with the kind and source
-                        vim_item.menu = "  "
-                            .. vim_item.kind
-                            .. " ("
-                            .. (source_map[entry.source.name] or entry.source.name)
-                            .. ")"
-                        vim_item.menu_hl_group = "SpecialComment"
-
-                        vim_item.abbr = ltrim(vim_item.abbr)
-
-                        if vim_item.kind == "Color" and entry.completion_item.documentation then
-                            local _, _, r, g, b =
-                                string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
-                            if r then
-                                local color = string.format("%02x", r)
-                                    .. string.format("%02x", g)
-                                    .. string.format("%02x", b)
-                                local group = "Tw_" .. color
-                                if vim.fn.hlID(group) < 1 then
-                                    vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
-                                end
-                                vim_item.kind_hl_group = group
-                                return vim_item
-                            end
-                        end
-
-                        return vim_item
-                    end,
-                },
+                fields = { "abbr", "kind", "menu" },
+                format = function(entry, vim_item)
+                    vim_item.menu = sources[entry.source.name] or entry.source.name
+                    return vim_item
+                end,
             },
         }
 
