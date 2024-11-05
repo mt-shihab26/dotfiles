c143b0821463c838ef838555fef35edd5ad84587
Shihab Mahamud <shihab4t@gmail.com>
2024-11-05 20:56:28 +0600

    if I paste don't copy the select remove text #18

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,10 @@
 local map = vim.keymap.set
 
+-- Clear search highlights with Esc
 map("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting" })
 
+-- Toggle between tabs/spaces
 map("n", "<leader>t", require "utils.toggle_indentation", { desc = "toggle tabs/spaces for indentation" })
+
+-- Paste without copying replaced text
+map("x", "p", '"_dP', { noremap = true, desc = "paste without yanking selected text" })
