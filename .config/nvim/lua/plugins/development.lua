1908ae2425cce88f643993c7353f425f75983260
Shihab Mahamud <shihab4t@gmail.com>
2024-12-06 00:24:40 +0600

    add sr.lua dev plugin

---
+++ .config/nvim/lua/plugins/development.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        dir = "~/Code/sr.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("sr").setup {
+                keymap = "<leader>sr",
+                ignore_case = false,
+                use_regex = false,
+                preview_changes = true,
+                live_preview = true,
+            }
+        end,
+    },
+}
