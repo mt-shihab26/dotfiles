2ea0307430caffe86c22df30f2876b6733ae26e5
Shihab Mahamud <shihab4t@gmail.com>
2024-12-10 13:03:23 +0600

    oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,15 @@
 return {
     "stevearc/oil.nvim",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
     dependencies = {
         "nvim-tree/nvim-web-devicons",
+    },
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        default_file_explorer = true,
+        view_options = {
+            show_hidden = true,
+        },
     },
     keys = {
         { "<leader>o", "<CMD>Oil<CR>", desc = "open oli plugin" },
