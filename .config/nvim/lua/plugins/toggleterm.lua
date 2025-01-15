93fde366c463c0a83a828589ff2589d9c04a94ab
Shihab Mahamud <shihab4t@gmail.com>
2025-01-15 23:49:12 +0600

    toggle term disabled

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,13 +1,12 @@
 return {
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        opts = {
-            open_mapping = [[<leader>k]],
-            direction = "float",
-            float_opts = {
-                border = "rounded",
-            },
+    enabled = false,
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+        open_mapping = [[<leader>k]],
+        direction = "float",
+        float_opts = {
+            border = "rounded",
         },
     },
 }
