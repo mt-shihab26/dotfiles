f3202c9c6dfca38946d39958d3dd46212869f3ed
Shihab Mahamud <shihab4t@gmail.com>
2025-01-29 05:16:00 +0600

    reordering

--- .config/nvim/lua/configs/lsp_servers.lua
+++ .config/nvim/lua/configs/lsp_servers.lua
@@ -1,18 +1,8 @@
 return {
-    -- HTML
+    -- TypeScript
     html = {},
-
-    -- CSS
     cssls = {},
     tailwindcss = require "settings.tailwindcss",
-
-    -- Lua
-    lua_ls = require "settings.lua_ls",
-
-    -- Python
-    pyright = require "settings.pyright",
-
-    -- Javascript
     vtsls = require "settings.vtsls",
     astro = {},
     volar = require "settings.volar",
@@ -21,22 +11,17 @@ return {
     -- PHP
     intelephense = {},
 
-    -- ruby
-    ruby_lsp = {},
-
     -- Elixir
     elixirls = {},
 
-    -- Go
-    gopls = require "settings.gopls",
-    templ = {},
-
-    -- RUST
-    rust_analyzer = {},
-
-    -- zig
+    -- Zig
     zls = {},
 
-    -- C/C++
+    -- Others
+    lua_ls = require "settings.lua_ls",
+    pyright = require "settings.pyright",
+    gopls = require "settings.gopls",
+    templ = {},
+    rust_analyzer = {},
     clangd = {},
 }
