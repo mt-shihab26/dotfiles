54506a7c36bab3a92c8e4be13c2aa3dfc6cf4ae2
Shihab Mahamud <shihab4t@gmail.com>
2025-02-26 22:59:01 +0600

    servers

--- .config/nvim/lua/configs/servers.lua
+++ .config/nvim/lua/configs/servers.lua
@@ -1,32 +1,47 @@
 return {
     html = {}, -- HTML
-    marksman = {}, -- Markdown
     htmx = {}, -- HTMX
+    marksman = {}, -- Markdown
+
     cssls = {}, -- CSS
     css_variables = {}, -- CSS
     tailwindcss = require "settings.tailwindcss", -- TailwindCSS
+
     jsonls = {}, -- Json
     yamlls = {}, -- YAML
+
     bashls = {}, -- Bash
+
     sqls = {}, -- SQL
+
     dockerls = {}, -- Docker
     docker_compose_language_service = {}, -- Docker
+
     vtsls = require "settings.vtsls", -- TypeScript
     volar = require "settings.volar", -- Vue
     astro = {}, -- Astro
     angularls = {}, -- Angular
     svelte = {}, -- Svelte
     prismals = {}, -- Prisma
+
     intelephense = {}, -- PHP
     stimulus_ls = {}, -- Blade
     twiggy_language_server = {}, -- Twig
+
     gopls = require "settings.gopls", -- Go
     templ = {}, -- Go
+
     lua_ls = require "settings.lua_ls", -- Lua
+
     ruby_lsp = {}, -- Ruby
+
     pyright = require "settings.pyright", -- Python
+
     elixirls = {}, -- Elixir
+
     rust_analyzer = {}, -- Rust
+
     zls = {}, -- Zig
+
     clangd = {}, -- C/C++
 }
