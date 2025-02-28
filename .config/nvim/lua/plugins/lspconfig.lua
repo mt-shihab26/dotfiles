a23e4a3edef85d02525fd115bfb6c689d5eaa41d
Shihab Mahamud <shihab4t@gmail.com>
2025-03-01 05:47:12 +0600

    dotfiles

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -141,5 +141,16 @@ return {
                 on_attach = on_attach,
             }, server_settings or {}))
         end
+
+        -- vim.api.nvim_create_autocmd("FileType", {
+        --     pattern = { "php", "blade" },
+        --     callback = function()
+        --         vim.lsp.start {
+        --             name = "laravel-ls",
+        --             cmd = { "laravel-ls" },
+        --             root_dir = vim.fn.getcwd(),
+        --         }
+        --     end,
+        -- })
     end,
 }
