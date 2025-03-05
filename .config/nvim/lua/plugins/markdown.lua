9693c42923e395d764f92b57aef7bcc7dab26b6b
Shihab Mahamud <shihab4t@gmail.com>
2025-03-05 18:51:17 +0600

    markdown

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,19 @@
 return {
-    -- Make sure to set this up properly if you have lazy=true
     "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-        file_types = { "markdown", "Avante" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
     },
-    ft = { "markdown", "Avante" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        file_types = {
+            "markdown",
+            "Avante",
+        },
+    },
+    ft = {
+        "markdown",
+        "Avante",
+    },
 }
