9ef706c23a2ce082a40133c2ce5bd8638c0f89f5
Shihab Mahamud <shihab4t@gmail.com>
2025-03-18 18:55:07 +0600

    copilot setup

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,9 @@
-return {}
-
--- return {
---     "zbirenbaum/copilot.lua",
---     cmd = "Copilot",
---     event = "InsertEnter",
---     config = function()
---         require("copilot").setup {}
---     end,
--- }
+return {
+    "github/copilot.vim",
+    config = function()
+        vim.cmd [[
+            imap <silent><script><expr> <A-CR> copilot#Accept("\<CR>")
+            let g:copilot_no_tab_map = v:true
+        ]]
+    end,
+}
