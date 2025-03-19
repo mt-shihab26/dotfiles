3e4bce9428092cfa60701116fb016a11bd54ee70
Shihab Mahamud <shihab4t@gmail.com>
2025-03-19 15:27:05 +0600

    inline code

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,34 +1,3 @@
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
-local tab_fallback = function(cmp, luasnip)
-    return function(fallback)
-        if cmp.visible() then
-            cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-        elseif has_words_before() then
-            cmp.complete()
-        else
-            fallback()
-        end
-    end
-end
-
-local shift_tab_fallback = function(cmp, luasnip)
-    return function(fallback)
-        if cmp.visible() then
-            cmp.select_prev_item()
-        elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-        else
-            fallback()
-        end
-    end
-end
-
 return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -50,17 +19,16 @@ return {
             panel = { enabled = false },
         }
 
-        -- Then set up copilot-cmp
         require("copilot_cmp").setup()
 
         local sources = {
+            copilot = "copilot",
             nvim_lsp = "lsp",
             nvim_lsp_signature_help = "signature",
             buffer = "buffer",
             path = "path",
             calc = "calc",
             luasnip = "snippet",
-            copilot = "copilot",
         }
 
         ---@type table
@@ -85,8 +53,39 @@ return {
             },
             mapping = {
                 ["<CR>"] = cmp.mapping.confirm { select = true },
-                ["<Tab>"] = cmp.mapping(tab_fallback(cmp, luasnip), { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(shift_tab_fallback(cmp, luasnip), { "i", "s" }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    -- Check if completion menu is visible
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    -- Check if snippet can be expanded or jumped to next position
+                    elseif luasnip.expand_or_jumpable() then
+                        vim.fn.feedkeys(
+                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                            ""
+                        )
+                    -- Check if there are words before cursor position
+                    elseif
+                        (function()
+                            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                            return col ~= 0
+                                and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s"
+                                    == nil
+                        end)()
+                    then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             },
             sources = {
                 { name = "copilot" },
