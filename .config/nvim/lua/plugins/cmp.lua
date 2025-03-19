392e425d2de4df8e1785989be6c9b181810faa07
Shihab Mahamud <shihab4t@gmail.com>
2025-03-20 00:42:51 +0600

    old cmp back

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,3 +1,34 @@
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local tab_fallback = function(cmp, luasnip)
+    return function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+        elseif has_words_before() then
+            cmp.complete()
+        else
+            fallback()
+        end
+    end
+end
+
+local shift_tab_fallback = function(cmp, luasnip)
+    return function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end
+end
+
 return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -10,25 +41,19 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "roobert/tailwindcss-colorizer-cmp.nvim",
-        "zbirenbaum/copilot.lua",
         "zbirenbaum/copilot-cmp",
     },
     config = function()
-        require("copilot").setup {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-        }
-
         require("copilot_cmp").setup()
 
         local sources = {
-            copilot = "copilot",
             nvim_lsp = "lsp",
             nvim_lsp_signature_help = "signature",
             buffer = "buffer",
             path = "path",
             calc = "calc",
             luasnip = "snippet",
+            copilot = "copilot",
         }
 
         ---@type table
@@ -53,48 +78,16 @@ return {
             },
             mapping = {
                 ["<CR>"] = cmp.mapping.confirm { select = true },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    -- Check if completion menu is visible
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    -- Check if snippet can be expanded or jumped to next position
-                    elseif luasnip.expand_or_jumpable() then
-                        vim.fn.feedkeys(
-                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-                            ""
-                        )
-                    -- Check if there are words before cursor position
-                    elseif
-                        (function()
-                            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                            return col ~= 0
-                                and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s"
-                                    == nil
-                        end)()
-                    then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                ["<Tab>"] = cmp.mapping(tab_fallback(cmp, luasnip), { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(shift_tab_fallback(cmp, luasnip), { "i", "s" }),
             },
             sources = {
                 { name = "nvim_lsp" },
                 { name = "nvim_lsp_signature_help" },
                 { name = "buffer" },
                 { name = "path" },
-                { name = "copilot" },
-                { name = "luasnip" },
                 { name = "calc" },
+                { name = "luasnip" },
             },
             formatting = {
                 fields = { "abbr", "kind", "menu" },
