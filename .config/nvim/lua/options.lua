d4d2a47799d18d7b140f4e690c9f276e27132378
Shihab Mahamud <shihab4t@gmail.com>
2025-04-07 03:58:56 +0600

    add docs to options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,45 +1,54 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Leader Key Configuration
+vim.g.mapleader = " " -- Set the leader key to space
+vim.g.maplocalleader = "\\" -- Set the local leader key to backslash
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Line numbering
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
 
+-- Visual indicators
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.signcolumn = "yes" -- Always show the sign column
+vim.opt.list = true -- Show invisible characters
+vim.opt.listchars = { space = "·", trail = "·", tab = "→ ", eol = "↲", nbsp = "␣" } -- Define how invisible chars appear
+
+-- Window behavior
+vim.opt.splitright = true -- Open new vertical splits to the right
+vim.opt.splitbelow = true -- Open new horizontal splits below
+vim.wo.wrap = false -- Don't wrap lines
+vim.opt.scrolloff = 10 -- Keep 10 lines visible above/below cursor
+
+-- Cursor style
+vim.opt.guicursor = "n-v-c:block-blinkwait700-blinkoff400-blinkon250,i-ci-ve:block-blinkwait700-blinkoff400-blinkon250" -- Block cursor in all modes with blinking, Wait 700ms before blinking, off for 400ms, on for 250ms
+
+-- Indentation settings
+vim.opt.tabstop = 4 -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for autoindent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.autoindent = true -- Copy indent from current line when starting a new line
+vim.opt.breakindent = true -- Wrapped lines preserve indentation
+
+-- Search settings
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.smartcase = true -- Override ignorecase if search contains uppercase
+vim.opt.inccommand = "split" -- Show preview of substitutions in a split
+
+-- Clipboard
 vim.schedule(function()
-    vim.opt.clipboard = "unnamedplus"
+    vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 end)
 
-vim.opt.mouse = "a"
+-- Mouse support
+vim.opt.mouse = "a" -- Enable mouse in all modes
 
-vim.opt.breakindent = true
+-- Files and Backup
+vim.opt.undofile = true -- Enable persistent undo history
+vim.opt.swapfile = false -- Disable swap files
 
-vim.opt.undofile = true
+-- Performance and Timing
+vim.opt.updatetime = 250 -- Faster completion and other operations
+vim.opt.timeoutlen = 300 -- Time to wait for a mapped sequence to complete
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.spelllang = "en_us"
-vim.opt.spell = true
-
-vim.opt.list = true
-vim.opt.listchars = { space = "·", trail = "·", tab = "→ ", eol = "↲", nbsp = "␣" }
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-
-vim.opt.swapfile = false
-
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-
-vim.wo.wrap = false
-
-vim.opt.guicursor = ""
-vim.opt.guicursor = "n-v-c:block-blinkwait700-blinkoff400-blinkon250,i-ci-ve:block-blinkwait700-blinkoff400-blinkon250"
+-- Spell Checking
+vim.opt.spelllang = "en_us" -- Set spelling language to US English
+vim.opt.spell = true -- Enable spell checking
