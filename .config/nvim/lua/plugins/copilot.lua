2c07ef2fc34e447a717e39a5414e159c2bb426b3
Shihab Mahamud <shihab4t@gmail.com>
2025-04-09 19:49:10 +0600

    wip

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,17 @@
-return {}
-
--- return {
---     "github/copilot.vim",
---     config = function()
---         vim.cmd [[
---             imap <silent><script><expr> <M-CR> copilot#Accept("\<CR>")
---             let g:copilot_no_tab_map = v:true
---         ]]
---     end,
--- }
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                keymap = {
+                    accept = "<Tab>",
+                    dismiss = "<C-]>",
+                },
+            },
+        }
+    end,
+}
