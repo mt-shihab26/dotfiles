e462d7d1f6663a9ef80f77db5d3530730a22b554
Shihab Mahamud <shihab4t@gmail.com>
2025-04-10 04:05:58 +0600

    bins setup

---
+++ .config/nvim/lua/plugins/bins.lua
@@ -0,0 +1,14 @@
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-tool-installer").setup {
+            ensure_installed = require "configs.bins",
+            auto_update = false,
+            run_on_start = false,
+        }
+    end,
+}
