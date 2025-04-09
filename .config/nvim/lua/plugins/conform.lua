9a32da9bc40a92f3aebef97a5c519b715cd79f83
Shihab Mahamud <shihab4t@gmail.com>
2025-04-10 04:27:56 +0600

    conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,41 @@
 return {
     "stevearc/conform.nvim",
     opts = {
-        formatters_by_ft = require "configs.formatters",
+        formatters_by_ft = {
+            lua = { "stylua" },
+            html = { "prettier" },
+            markdown = { "prettier" },
+            mdx = { "prettier" },
+            svg = { "prettier" },
+            css = { "prettier" },
+            scss = { "prettier" },
+            less = { "prettier" },
+            json = { "prettier" },
+            jsonc = { "prettier" },
+            yaml = { "prettier" },
+
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            astro = { "prettier" },
+            vue = { "prettier" },
+
+            php = { "pint" },
+            blade = { "prettier" },
+
+            ruby = { "rubyfmt" },
+
+            python = { "ruff" },
+
+            go = { "goimports", "gofmt" },
+            templ = { "templ" },
+
+            rust = { "rustfmt" },
+        },
         format_on_save = {
             timeout_ms = 5000,
-            -- lsp_format = "fallback",
+            lsp_format = "fallback",
         },
     },
 }
