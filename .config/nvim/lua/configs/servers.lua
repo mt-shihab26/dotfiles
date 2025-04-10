5d75cfe51d80e98d36f7d4dd56cb1b7427b0a905
Shihab Mahamud <shihab4t@gmail.com>
2025-04-10 15:02:36 +0600

    servers

--- .config/nvim/lua/configs/servers.lua
+++ .config/nvim/lua/configs/servers.lua
@@ -1,37 +1,45 @@
 return {
     lua_ls = require "settings.lua_ls", -- Lua
+
     html = {}, -- HTML
     marksman = {}, -- Markdown
+
     cssls = {}, -- CSS
     css_variables = {}, -- CSS
     tailwindcss = require "settings.tailwindcss", -- TailwindCSS
+
     jsonls = {}, -- Json
     yamlls = {}, -- YAML
+
     bashls = {}, -- Bash
+
     sqls = {}, -- SQL
+
     dockerls = {}, -- Docker
     docker_compose_language_service = {}, -- Docker
 
     vtsls = require "settings.vtsls", -- TypeScript
     volar = require "settings.volar", -- Vue
     astro = {}, -- Astro
-    -- angularls = {}, -- Angular
-    -- svelte = {}, -- Svelte
-    -- prismals = {}, -- Prisma
+    htmx = {}, -- HTMX
+    angularls = {}, -- Angular
+    svelte = {}, -- Svelte
+    prismals = {}, -- Prisma
 
     intelephense = {}, -- PHP
-    -- stimulus_ls = {}, -- Blade
-    -- twiggy_language_server = {}, -- Twig
 
     ruby_lsp = {}, -- Ruby
+
+    pyright = require "settings.pyright", -- Python
+
+    elixirls = {}, -- Elixir
 
     gopls = require "settings.gopls", -- Go
     templ = {}, -- Go
 
-    -- htmx = {}, -- HTMX
-    -- pyright = require "settings.pyright", -- Python
-    -- elixirls = {}, -- Elixir
-    -- rust_analyzer = {}, -- Rust
-    -- zls = {}, -- Zig
-    -- clangd = {}, -- C/C++
+    rust_analyzer = {}, -- Rust
+
+    zls = {}, -- Zig
+
+    clangd = {}, -- C/C++
 }
