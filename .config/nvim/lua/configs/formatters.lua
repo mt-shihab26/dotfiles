1f7c29be25fb16ddab4190bb8196ca52044f6230
Shihab Mahamud <shihab4t@gmail.com>
2025-04-10 15:05:30 +0600

    formatters

---
+++ .config/nvim/lua/configs/formatters.lua
@@ -0,0 +1,45 @@
+return {
+    lua = { "stylua" },
+
+    sh = { "shfmt" },
+    bash = { "shfmt" },
+    zsh = { "shfmt" },
+
+    sql = { "sqlfmt" },
+
+    html = { "prettier" },
+    markdown = { "prettier" },
+    mdx = { "prettier" },
+    svg = { "prettier" },
+    css = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    toml = { "prettier" },
+
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    astro = { "prettier" },
+    vue = { "prettier" },
+
+    php = { "pint" },
+    blade = { "prettier" },
+
+    ruby = { "rubyfmt" },
+
+    python = { "ruff" },
+
+    go = { "goimports", "gofmt" },
+    templ = { "templ" },
+
+    swift = { "swiftformat" },
+
+    rust = { "rustfmt" },
+
+    zig = { "zigfmt" },
+
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+    cmake = { "cmake_format" },
+}
