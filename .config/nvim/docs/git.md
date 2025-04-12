99611d2e7b412f41c79036c18e4b11a925b02f43
Shihab Mahamud <shihab4t@gmail.com>
2025-04-12 21:33:38 +0600

    git plugins

--- .config/nvim/docs/git.md
+++ .config/nvim/docs/git.md
@@ -1,6 +1,20 @@
+# Git
+
 ## LazyGit
 
 - commands
     - `:LazyGit`
 - keymaps:
     - `<leader>z` -> open lazygit
+
+## vim-fugitive
+
+- commands
+    - `:Git`
+
+## diffview.nvim
+
+- commands
+    - `:DiffviewOpen`
+- keymaps
+    - `<leader>d` -> open diff view
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,32 @@
 return {
     {
+        "lewis6991/gitsigns.nvim",
+        lazy = false,
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "┆" },
+            },
+            signs_staged = {
+                add = { text = "│" },
+                change = { text = "≈" },
+                delete = { text = "▁" },
+                topdelete = { text = "▔" },
+                changedelete = { text = "≋" },
+                untracked = { text = "║" },
+            },
+            current_line_blame = false,
+            current_line_blame_opts = {
+                virt_text = true,
+                delay = 500,
+            },
+        },
+    },
+    {
         "kdheepak/lazygit.nvim",
         dependencies = {
             "nvim-telescope/telescope.nvim",
@@ -14,5 +41,11 @@ return {
     },
     {
         "tpope/vim-fugitive",
+    },
+    {
+        "sindrets/diffview.nvim",
+        keys = {
+            { "<leader>d", "<cmd>DiffviewOpen<cr>", desc = "open diff view", remap = true },
+        },
     },
 }
