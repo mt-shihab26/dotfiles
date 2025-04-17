be8dfdece54c85115b308174abdaae986a7fb861
Shihab Mahamud <shihab4t@gmail.com>
2025-04-17 20:41:11 +0600

    oil nvim config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,28 +2,29 @@ return {
     "stevearc/oil.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
+        "refractalize/oil-git-status.nvim",
     },
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-        default_file_explorer = true,
-        view_options = {
-            show_hidden = true,
-        },
-        -- Enable git status in the UI
-        columns = {
-            "icon",
-            "permissions",
-            "size",
-            "mtime",
-            -- Add git status column
-            "git_status",
-        },
-        -- Configure git integration for highlighting
-        win_options = {
-            signcolumn = "yes", -- Show signcolumn for git status
-        },
-    },
+    config = function()
+        require("oil").setup {
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            },
+            columns = {
+                "icon",
+                "permissions",
+                "size",
+                "mtime",
+            },
+            win_options = {
+                signcolumn = "yes:2",
+            },
+        }
+
+        require("oil-git-status").setup {
+            --
+        }
+    end,
     keys = {
         { "-", "<CMD>Oil<CR>", desc = "open directories as buffer (oil)" },
     },
