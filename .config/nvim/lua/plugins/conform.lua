394075e5d06d084e53167eea276534b2d25a13f0
Shihab Mahamud <shihab4t@gmail.com>
2025-04-30 16:55:12 +0600

    nvim config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,4 @@
-return {
+local formatters = {
     formatters_by_ft = {
         lua = { "stylua" },
 
@@ -57,4 +57,24 @@ return {
 
         "cmake_format",
     },
+}
+
+return {
+    "stevearc/conform.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "zapling/mason-conform.nvim",
+    },
+    config = function()
+        require("conform").setup {
+            formatters_by_ft = formatters.formatters_by_ft,
+            format_on_save = { timeout_ms = 5000, lsp_format = "fallback" },
+        }
+
+        require("mason").setup()
+
+        require("mason-conform").setup {
+            ignore_install = formatters.ignore_install,
+        }
+    end,
 }
