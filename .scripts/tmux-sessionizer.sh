1425d6fd0397afc56b1b5b5305cdd04dd6046dc0
Shihab Mahamud <shihab4t@gmail.com>
2025-05-07 14:16:30 +0600

    wip

--- .scripts/tmux-sessionizer.sh
+++ .scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "projects" ]]; then
     selected=$(find ~/projects -mindepth 1 -maxdepth 1 -type d | fzf)
 elif [[ $1 == "learns" ]]; then
-    selected=$(find ~/learns/courses/ ~/learns/crash_courses/ ~/learns/imaginative_research/ ~/learns/projects/ ~/learns/manual/ -mindepth 1 -maxdepth 2 -type d | fzf)
+    selected=$(find ~/learns/ ~/learns/courses/ ~/learns/crash_courses/ ~/learns/imaginative_research/ ~/learns/projects/ ~/learns/manual/ -mindepth 1 -maxdepth 2 -type d | fzf)
 elif [[ $# -eq 1 ]]; then
     selected=$1
 else
