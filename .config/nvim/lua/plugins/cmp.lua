fb24cd1f35f618667d0a6e41240dca2f593f4e49
Shihab Mahamud <shihab4t@gmail.com>
2025-05-08 15:57:09 +0600

    enable copilot

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -35,7 +35,7 @@ return {
                 suggestion = { enabled = false },
                 panel = { enabled = false },
             }
-            vim.cmd "Copilot disable"
+            -- vim.cmd "Copilot disable"
         end,
     },
     {
