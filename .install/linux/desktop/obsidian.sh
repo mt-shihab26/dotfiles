60c0435e8b318122a54b3e60f6071b1c7e4d6041
Shihab Mahamud <shihab4t@gmail.com>
2025-05-25 02:30:36 +0600

    obsidian

---
+++ .install/linux/desktop/obsidian.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -e
+
+command="obsidian"
+
+if ! command -v "$command" &>/dev/null; then
+    echo -e "Checking for '$command'... not found."
+    echo -e "Downloading and installing '$command'..."
+
+    cd /tmp
+
+    echo -e "\nDownloading Obsidian .deb package..."
+    wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.10/obsidian_1.8.10_amd64.deb"
+
+    echo -e "\nInstalling Obsidian..."
+    sudo apt install -y ./obsidian.deb
+
+    echo -e "\nCleaning up..."
+    rm ./obsidian.deb
+    cd -
+
+    echo -e "\n'$command' has been successfully installed."
+else
+    echo -e "'$command' is already installed on this system."
+fi
