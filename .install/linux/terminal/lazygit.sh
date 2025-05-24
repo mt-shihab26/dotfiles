77a3658c03ef0d1422628608261bb6ceb75ae87f
Shihab Mahamud <shihab4t@gmail.com>
2025-05-24 06:40:01 +0600

    update echo logs

--- .install/linux/terminal/lazygit.sh
+++ .install/linux/terminal/lazygit.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+echo -e "\nInstalling lazygit..."
+
+echo -e "\nDownloading and installing lazygit..."
 cd /tmp
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -5,3 +10,5 @@ tar -xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm lazygit.tar.gz lazygit
 cd ~
+
+echo -e "\nlazygit installation complete.\n"
--- .install/linux/terminal/mysql.sh
+++ .install/linux/terminal/mysql.sh
@@ -2,22 +2,23 @@
 
 MYSQL_ROOT_PASSWORD="2611"
 
+echo -e "\nUpdating package list..."
 sudo apt update
 
-echo "[INFO] Pre-configuring MySQL root password..."
+echo -e "\nPre-configuring MySQL root password..."
 echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
 
-echo "[INFO] Installing MySQL Server..."
+echo -e "\nInstalling MySQL Server..."
 sudo DEBIAN_FRONTEND=noninteractive apt install -y mysql-server
 
-echo "[INFO] Configuring MySQL root user password..."
+echo -e "\nConfiguring MySQL root user password..."
 sudo mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
-echo "[INFO] Securing MySQL installation..."
+echo -e "\nSecuring MySQL installation..."
 sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
@@ -25,8 +26,8 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
 
-echo "[INFO] Enabling and starting MySQL service..."
+echo -e "\nEnabling and starting MySQL service..."
 sudo systemctl enable mysql
 sudo systemctl start mysql
 
-echo "[SUCCESS] MySQL installation and configuration completed."
+echo -e "\nMySQL installation and configuration completed.\n"
