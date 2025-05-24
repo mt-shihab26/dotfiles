6a0893f01ec97f4b13ac9977a55545f9beecfd38
Shihab Mahamud <shihab4t@gmail.com>
2025-05-25 05:20:10 +0600

    redis install script

---
+++ .install/linux/terminal/redis.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+set -e
+
+echo -e "Updating package index..."
+sudo apt update
+
+echo -e "\nInstalling Redis server..."
+sudo apt install -y redis-server
+
+echo -e "\nEnabling and starting Redis service..."
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
+
+echo -e "\nChecking Redis status..."
+sudo systemctl status redis-server | grep Active
+
+echo -e "\nTesting Redis with ping..."
+redis-cli ping
+
+echo -e "\nRedis installation and basic check complete."
