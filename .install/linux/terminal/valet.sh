610ed7dbc10551b8f8ef7c15d430fbbd32f81223
Shihab Mahamud <shihab4t@gmail.com>
2025-05-27 02:53:07 +0600

    wip

--- .install/linux/terminal/valet.sh
+++ .install/linux/terminal/valet.sh
@@ -12,7 +12,7 @@ echo -e "\nUpdating package lists..."
 sudo nala update
 
 echo -e "\nInstalling required system packages for Valet Linux..."
-sudo nala install -y network-manager libnss3-tools jq xsel nginx
+sudo nala install -y network-manager libnss3-tools jq xsel nginx dnsmasq inotify-tools
 
 echo -e "\nInstalling Valet Linux globally via Composer..."
 composer global require cpriego/valet-linux
