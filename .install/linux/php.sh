8e9371b28a30cddd4def00de5dea2d4540147256
Shihab Mahamud <shihab4t@gmail.com>
2025-05-29 01:00:32 +0000

    php setup

---
+++ .install/linux/php.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo pacman -S php \
+    php-fpm \
+    php-gd \
+    php-intl \
+    php-mysqli \
+    php-mysql \
+    php-sqlite3 \
+    php-redis \
+    php-xdebug
+
+# php-bcmath \
+# php-bz2 \
+# php-gmp \
