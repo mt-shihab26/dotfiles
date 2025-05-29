cd39951317272243f28852478571b7eee2c3089c
Shihab Mahamud <shihab4t@gmail.com>
2025-05-29 21:07:07 +0600

    php

--- .install/linux/php.sh
+++ .install/linux/php.sh
@@ -16,10 +16,18 @@ composer global require cpriego/valet-linux
 valet install
 
 echo "\nAll the PHP extension have been installed."
+echo "ls /usr/lib/php/modules"
 ls /usr/lib/php/modules
+
+echo ""
+php --ini
 
 echo "
 # Add this to php.ini file
+
+memory_limit = 512M
+max_execution_time = 300
+max_input_time = 300
 
 extension=mysqli
 "
