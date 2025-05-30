78f75c099e89e76bd91866607fdf074b1021610f
Shihab Mahamud <shihab4t@gmail.com>
2025-05-30 07:04:09 +0600

    gtk

--- .install/linux/gtk-apps.sh
+++ .install/linux/gtk-apps.sh
@@ -1,2 +1,9 @@
-yay -S dracula-gtk-theme
+sudo pacman -S sassc gnome-themes-extra gtk-engine-murrine
+
+git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git /tmp/TokyoNight
+bash /tmp/TokyoNight/themes/install.sh
+
 sudo pacman -S nwg-look nautilus loupe totem fragments decibels snapshot gnome-clocks evince gedit
+
+gsettings set org.gnome.desktop.interface gtk-theme "Tokyonight-Dark"
+gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
