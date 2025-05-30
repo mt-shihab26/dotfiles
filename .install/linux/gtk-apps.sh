3332322b0c272c8425369fe41ac717abf57d3ee5
Shihab Mahamud <shihab4t@gmail.com>
2025-05-30 06:28:15 +0600

    gtk theme

--- .install/linux/gtk-apps.sh
+++ .install/linux/gtk-apps.sh
@@ -1,1 +1,2 @@
-sudo pacman -S nwg-look nautilus loupe totem fragments decibels snapshot
+yay -S dracula-gtk-theme
+sudo pacman -S nwg-look nautilus loupe totem fragments decibels snapshot gnome-clocks evince gedit
