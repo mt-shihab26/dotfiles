708317e41f094bbe1738fbd5a58b83e1ae016870
Shihab Mahamud <shihab4t@gmail.com>
2025-06-01 03:57:16 +0600

    wip

--- .install/linux/sddm.sh
+++ .install/linux/sddm.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-yay -S sddm-astronaut-theme
+echo "Installing the sddm-astronaut-theme with yay..."
+# yay -S sddm-astronaut-theme
 
+echo "Listing available SDDM themes..."
 ls /usr/share/sddm/themes/
 
+echo "Linking your custom sddm.conf to /etc/sddm.conf..."
 sudo ln -s $HOME/.config/sddm/sddm.conf /etc/sddm.conf
 
+echo "Displaying the contents of /etc/sddm.conf..."
 cat /etc/sddm.conf
+
+echo "============================================="
+echo "TO SELECT A THEME:"
+echo "Edit the following file:"
+echo "/usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop"
+echo
+echo "Find and add this line:"
+echo "ConfigFile=Themes/hyprland_kath.conf"
+echo
+echo "Available theme configurations:"
+ls /usr/share/sddm/themes/sddm-astronaut-theme/Themes/
+echo "============================================="
