2b0bb33300195bcd5acb21db966a1c3140146376
Shihab Mahamud <shihab4t@gmail.com>
2025-06-02 05:03:07 +0600

    hyprlock conf

--- .install/linux/hyprland.sh
+++ .install/linux/hyprland.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S uwsm libnewt hyprland libnotify swaync hyprlock hypridle wpaperd
+sudo pacman -S uwsm libnewt hyprland libnotify swaync hyprlock hypridle wpaperd inetutils
 
 # Screenshot
 yay -S hyprshot
