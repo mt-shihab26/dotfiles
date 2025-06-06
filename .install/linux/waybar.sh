12a873dea42e32e91422d93404c0c5eaea33fcc0
Shihab Mahamud <shihab4t@gmail.com>
2025-06-06 07:19:11 +0600

    hyprland

--- .install/linux/waybar.sh
+++ .install/linux/waybar.sh
@@ -16,11 +16,3 @@ sudo systemctl start bluetooth
 
 # Volume Control
 sudo pacman -S pipewire wireplumber pavucontrol pamixer
-
-# Network Manager
-# sudo pacman -S iwd
-# yay -S iwgtk
-
-# sudo systemctl enable iwd
-# sudo systemctl start iwd
-
