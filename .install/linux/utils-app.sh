53e6a846826d918b1fc55d86e0ba97a66de04878
Shihab Mahamud <shihab4t@gmail.com>
2025-06-12 09:04:23 +0600

    utils app

---
+++ .install/linux/utils-app.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Torrent Client
+sudo pacman -S fragments
+
+# Image Previewer
+sudo pacman -S loupe
+
+# Audio Player
+sudo pacman -S decibels
+
+# Document Viewer
+sudo pacman -S evince
