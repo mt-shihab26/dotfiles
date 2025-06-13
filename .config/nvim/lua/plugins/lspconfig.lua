d0afa53c45d396048e6000c9ffa14f364103e5de
Shihab Mahamud <shihab4t@gmail.com>
2025-06-13 20:55:18 +0600

    lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -54,7 +54,6 @@ return {
         local mason_lspconfig = require "mason-lspconfig"
         local cmp_nvim_lsp = require "cmp_nvim_lsp"
         local lsp_file_operations = require "lsp-file-operations"
-        -- local which_key = require "which-key"
 
         mason.setup {}
         mason_lspconfig.setup { ensure_installed = vim.tbl_keys(servers), automatic_installation = true }
@@ -85,44 +84,6 @@ return {
             ),
         })
 
-        -- local function restart_all_lsp()
-        --     local active_clients = vim.lsp.get_active_clients()
-        --
-        --     if #active_clients == 0 then
-        --         vim.notify("No active LSP clients found", vim.log.levels.WARN)
-        --         return
-        --     end
-        --
-        --     for _, client in ipairs(active_clients) do
-        --         vim.lsp.stop_client(client.id)
-        --         vim.notify(string.format("Stopped LSP client: %s", client.name), vim.log.levels.INFO)
-        --     end
-        --
-        --     vim.defer_fn(function()
-        --         vim.cmd "edit"
-        --         vim.notify("LSP servers restarted", vim.log.levels.INFO)
-        --     end, 100)
-        -- end
-
-        -- local function stop_all_lsp()
-        --     local active_clients = vim.lsp.get_active_clients()
-        --     if #active_clients == 0 then
-        --         vim.notify("No active LSP clients found", vim.log.levels.WARN)
-        --         return
-        --     end
-        --
-        --     for _, client in ipairs(active_clients) do
-        --         vim.lsp.stop_client(client.id)
-        --         vim.notify(string.format("Stopped LSP client: %s", client.name), vim.log.levels.INFO)
-        --     end
-        --     vim.notify("All LSP servers stopped", vim.log.levels.INFO)
-        -- end
-
-        -- local function start_all_lsp()
-        --     vim.cmd "edit"
-        --     vim.notify("Starting LSP servers...", vim.log.levels.INFO)
-        -- end
-
         local on_attach = function(_, bufnr)
             local opts = function(opts)
                 return vim.tbl_extend("force", {
@@ -131,24 +92,6 @@ return {
                     buffer = bufnr,
                 }, opts or {})
             end
-
-            -- local restart_buffer_lsp = function()
-            --     local clients = vim.lsp.get_active_clients { bufnr = bufnr }
-            --     for _, client in ipairs(clients) do
-            --         vim.lsp.buf_request(bufnr, "workspace/executeCommand", {
-            --             command = "_typescript.restartServer",
-            --         })
-            --         vim.notify(string.format("Restarting LSP: %s", client.name))
-            --     end
-            -- end
-            --
-            -- local stop_buffer_lsp = function()
-            --     local clients = vim.lsp.get_active_clients { bufnr = bufnr }
-            --     for _, client in ipairs(clients) do
-            --         vim.lsp.stop_client(client.id)
-            --         vim.notify(string.format("Stopped LSP client: %s", client.name), vim.log.levels.INFO)
-            --     end
-            -- end
 
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts { desc = "go to definition (lspconfig)" })
             vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts { desc = "go to declaration (lspconfig)" })
@@ -164,24 +107,16 @@ return {
             )
 
             vim.keymap.set("n", "<leader>h", vim.lsp.buf.signature_help, opts { desc = "signature help (lspconfig)" })
-            vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts { desc = "code actions (lspconfig)" })
+            vim.keymap.set(
+                "n",
+                "<leader>a",
+                vim.lsp.buf.code_action,
+                { noremap = true, silent = true, desc = "code actions (lspconfig)" }
+            )
             vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts { desc = "rename symbol (lspconfig)" })
 
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts { desc = "go to prev diagnostic (lspconfig)" })
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts { desc = "go to next diagnostic (lspconfig)" })
-
-            -- which_key.add { { "<leader>L", group = "lsp servers management (lspconfig)" } }
-            --
-            -- vim.keymap.set("n", "<leader>Ls", stop_buffer_lsp, opts { desc = "stop buffer lsp servers (lspconfig)" })
-            -- vim.keymap.set("n", "<leader>LS", stop_all_lsp, opts { desc = "stop all lsp servers (lspconfig)" })
-            -- vim.keymap.set("n", "<leader>Ll", start_all_lsp, opts { desc = "start all lsp servers (lspconfig)" })
-            -- vim.keymap.set(
-            --     "n",
-            --     "<leader>Lr",
-            --     restart_buffer_lsp,
-            --     opts { desc = "restart buffer buffer servers (lspconfig)" }
-            -- )
-            -- vim.keymap.set("n", "<leader>LR", restart_all_lsp, opts { desc = "restart all lsp servers (lspconfig)" })
         end
 
         for server_name, server_settings in pairs(servers) do
@@ -190,16 +125,5 @@ return {
                 on_attach = on_attach,
             }, server_settings or {}))
         end
-
-        -- vim.api.nvim_create_autocmd("FileType", {
-        --     pattern = { "php", "blade" },
-        --     callback = function()
-        --         vim.lsp.start {
-        --             name = "laravel-ls",
-        --             cmd = { "laravel-ls" },
-        --             root_dir = vim.fn.getcwd(),
-        --         }
-        --     end,
-        -- })
     end,
 }
