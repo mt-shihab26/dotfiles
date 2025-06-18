ba73fffa8e104f94acac8290736f7ea2e0ef619f
Shihab Mahamud <shihab4t@gmail.com>
2025-06-18 09:57:45 +0600

    dap plugin

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -7,48 +7,55 @@ return {
     },
     config = function()
         local dap = require "dap"
-        local dapui = require "dapui"
-        local dapgo = require "dap-go"
+        local dap_ui = require "dapui"
+        local dap_go = require "dap-go"
+        local dap_ui_widgets = require "dap.ui.widgets"
 
-        dapui.setup()
-        dapgo.setup()
+        dap_ui.setup()
+        dap_go.setup()
 
-        dap.listeners.before.attach.dapui_config = dapui.open
-        dap.listeners.before.launch.dapui_config = dapui.open
-        dap.listeners.before.event_terminated.dapui_config = dapui.close
-        dap.listeners.before.event_exited.dapui_config = dapui.close
+        dap.listeners.before.attach.dapui_config = dap_ui.open
+        dap.listeners.before.launch.dapui_config = dap_ui.open
+        dap.listeners.before.event_terminated.dapui_config = dap_ui.close
+        dap.listeners.before.event_exited.dapui_config = dap_ui.close
 
-        local opts = function(opts)
-            return vim.tbl_extend("force", { noremap = true, silent = true, remap = false }, opts or {})
+        local opts = function(desc)
+            return { noremap = true, silent = true, remap = false, desc = desc }
+        end
+
+        local set_log_print = function()
+            dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
+        end
+
+        local show_frames = function()
+            dap_ui_widgets.centered_float(dap_ui_widgets.frames)
+        end
+
+        local show_scopes = function()
+            dap_ui_widgets.centered_float(dap_ui_widgets.scopes)
         end
 
         local map = vim.keymap.set
 
-        map("n", "<leader>dt", dap.toggle_breakpoint, opts { desc = "toggle breakpoint (dap)" })
-        map("n", "<leader>dc", dap.continue, { desc = "continue debugger (dap)" })
+        -- break point management
+        map("n", "<leader>dt", dap.toggle_breakpoint, opts "toggle breakpoint (dap)")
+        map("n", "<leader>db", dap.set_breakpoint, opts "set breakpoint (dap)")
+        map("n", "<leader>dB", set_log_print, opts "set log point (dap)")
+
+        -- step controls
+        map("n", "<leader>dc", dap.continue, opts "continue debugger (dap)")
+        map("n", "<leader>do", dap.step_over, opts "step over (dap)")
+        map("n", "<leader>di", dap.step_into, opts "step into (dap)")
+        map("n", "<leader>du", dap.step_out, opts "step out (dap)")
+
+        -- debug session management
+        map("n", "<leader>dr", dap.repl.open, opts "open REPL (dap)")
+        map("n", "<leader>dl", dap.run_last, opts "run last (dap)")
+
+        -- widgets
+        map({ "n", "v" }, "<leader>dh", dap_ui_widgets.hover, opts "hover widgets (dap)")
+        map({ "n", "v" }, "<leader>dp", dap_ui_widgets.preview, opts "preview widgets (dap)")
+        map("n", "<leader>df", show_frames, opts "show frames (dap)")
+        map("n", "<leader>ds", show_scopes, opts "show scopes (dap)")
     end,
 }
-
--- map("n", "<F10>", dap.step_over)
--- map("n", "<F11>", dap.step_into)
--- map("n", "<F12>", dap.step_out)
--- map("n", "<Leader>B", dap.set_breakpoint)
--- map("n", "<Leader>lp", function()
---     dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
--- end)
--- map("n", "<Leader>dr", dap.repl.open())
--- map("n", "<Leader>dl", dap.run_last())
--- map({ "n", "v" }, "<Leader>dh", function()
---     require("dap.ui.widgets").hover()
--- end)
--- map({ "n", "v" }, "<Leader>dp", function()
---     require("dap.ui.widgets").preview()
--- end)
--- map("n", "<Leader>df", function()
---     local widgets = require "dap.ui.widgets"
---     widgets.centered_float(widgets.frames)
--- end)
--- map("n", "<Leader>ds", function()
---     local widgets = require "dap.ui.widgets"
---     widgets.centered_float(widgets.scopes)
--- end)
--- .config/nvim/lua/plugins/tool-installer.lua
+++ .config/nvim/lua/plugins/tool-installer.lua
@@ -9,6 +9,7 @@ return {
         require("mason-tool-installer").setup {
             ensure_installed = {
                 "delve",
+                "codelldb",
             },
             auto_update = false,
             run_on_start = true,
