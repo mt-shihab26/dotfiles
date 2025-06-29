6b6e18f19a271b86428e80cd6d328177a4e50f04
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 21:53:38 +0600

    bindings done

--- .install/linux/hypr.sh
+++ .install/linux/hypr.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S hyprland swaync hyprshot hyprpicker hyprlock hypridle hyprpolkitagent btop fastfetch net-tools inetutils
+sudo pacman -S hyprland swaync hyprshot hyprpicker hyprlock hypridle hyprpolkitagent btop fastfetch net-tools inetutils wl-clip-persist
+
+yay -S clipse
