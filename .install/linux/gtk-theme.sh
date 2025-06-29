b19c74c2c519132114c3a8b775d8cc4126c57929
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 23:41:30 +0600

    catppuccin

--- .install/linux/gtk-theme.sh
+++ .install/linux/gtk-theme.sh
@@ -4,5 +4,5 @@ sudo pacman -S gnome-keyring gnome-themes-extra xdg-desktop-portal-gtk
 
 yay -S catppuccin-gtk-theme-mocha
 
-gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-rosewater-standard+default"
+gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-green-standard+default"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
