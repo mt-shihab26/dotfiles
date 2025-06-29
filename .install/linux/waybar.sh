6e6fcca0e311ac094010e453e97a8186219e1bc9
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 16:31:39 +0600

    rofi

--- .install/linux/waybar.sh
+++ .install/linux/waybar.sh
@@ -2,17 +2,8 @@
 
 sudo pacman -S waybar
 
-# Logout Manager
-yay -S wlogout wayland-logout
-
 # Birghtness Control
 sudo pacman -S brightnessctl gammastep
 
-# # Bluetooth Manager
-# sudo pacman -S blueman
-#
-# sudo systemctl enable bluetooth
-# sudo systemctl start bluetooth
-#
-# # Volume Control
-# sudo pacman -S pipewire wireplumber pavucontrol pamixer
+# Logout Manager
+yay -S wlogout wayland-logout
