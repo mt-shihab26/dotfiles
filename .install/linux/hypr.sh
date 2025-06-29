0f2c5fa7d4836a7e86abcc53f52ccfbfa20fc432
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 21:12:58 +0600

    hyprland

--- .install/linux/hypr.sh
+++ .install/linux/hypr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S hyprland swaync hyprshot hyprpicker hyprlock hypridle hyprpolkitagent btop fastfetch
+sudo pacman -S hyprland swaync hyprshot hyprpicker hyprlock hypridle hyprpolkitagent btop fastfetch net-tools inetutils
