e4def9b49b5785cb5cd8eac7e5982904390e6bb3
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 17:33:48 +0600

    probhat setup

--- .install/linux/fonts.sh
+++ .install/linux/fonts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S ttf-cascadia-mono-nerd ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-indic-otf 
+sudo pacman -S ttf-cascadia-mono-nerd ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-indic-otf
+yay -S ttf-freebanglafont
 
 fc-cache -fv
