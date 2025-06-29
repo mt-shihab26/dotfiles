498154d40e83b80befa5f88efb893222259c6a6b
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 19:42:09 +0600

    php

--- .install/linux/php.sh
+++ .install/linux/php.sh
@@ -23,11 +23,16 @@ composer global require laravel/pint
 composer global require laravel/installer
 composer global require cpriego/valet-linux
 
-echo "===================================="
-echo "Running Valet installation..."
-echo "===================================="
+# echo "===================================="
+# echo "Running Valet installation..."
+# echo "===================================="
 
-valet install
+# valet install
+
+echo "===================================="
+echo "PHP loaded modules:"
+echo "===================================="
+php -m
 
 echo "===================================="
 echo "✅ All PHP extensions have been installed."
@@ -41,12 +46,7 @@ echo "===================================="
 php --ini
 
 echo "===================================="
-echo " 🔍 PHP loaded modules:"
-echo "===================================="
-php -m
-
-echo "===================================="
-echo "📝 Please add the following settings to your php.ini file:"
+echo "Please add the following settings to your php.ini file:"
 echo "------------------------------------"
 echo "memory_limit = 2048M"
 echo "max_execution_time = 24000"
@@ -61,4 +61,8 @@ echo "extension=bcmath"
 echo "extension=igbinary"
 echo "extension=redis"
 echo "extension=iconv"
+echo "===================================="
+
+echo "===================================="
+echo " Then run '\$ valet install'"
 echo "===================================="
--- .install/linux/probhat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S xkeyboard-config
-localectl list-x11-keymap-layouts | grep bd
-localectl list-x11-keymap-variants bd
-setxkbmap -layout bd -variant probhat
