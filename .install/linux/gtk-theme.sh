b76fd544bce4f900abba6f09bbe6110380e061ec
Shihab Mahamud <shihab4t@gmail.com>
2025-06-29 20:45:39 +0600

    wip

--- .install/linux/gtk-theme.sh
+++ .install/linux/gtk-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S gnome-themes-extra xdg-desktop-portal-gtk # Adds Adwaita-dark theme
+sudo pacman -S gnome-keyring gnome-themes-extra xdg-desktop-portal-gtk
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
---
+++ .install/linux/wallpapers.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo pacman -S wpaperd
+
+git clone --depth=1 https://github.com/mt-shihab26/wallpapers.git ~/wallpapers
