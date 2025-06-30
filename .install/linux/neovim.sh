6f6893dfb841c6a6a254cc39c4355baacbed8a6b
Shihab Mahamud <shihab4t@gmail.com>
2025-06-30 16:20:32 +0600

    fixing neovim

--- .install/linux/neovim.sh
+++ .install/linux/neovim.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S neovim lazygit wl-clipboard lua luarocks ripgrep imagemagick nodejs npm pnpm
+sudo pacman -S neovim lazygit wl-clipboard lua luarocks imagemagick nodejs npm pnpm ripgrep fd base-devel gcc clang
