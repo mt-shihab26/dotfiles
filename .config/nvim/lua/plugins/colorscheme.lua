35f6f38b413876e9157256241d68869222b0e32b
Shihab Mahamud <shihab4t@gmail.com>
2025-08-03 09:05:24 +0600

    neovim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+    {
+        "NvChad/nvim-colorizer.lua",
+        opts = {
+            user_default_options = {
+                names = false,
+                tailwind = true,
+            },
+        },
+    },
+    {
+        "xiyaowong/transparent.nvim",
+        -- enabled = false,
+        config = function()
+            require("transparent").setup {
+                groups = {
+                    "Normal",
+                    "NormalNC",
+                    "NeoTreeNormal",
+                    "NeoTreeNormalNC",
+                    "TelescopeNormal",
+                    "TelescopeBorder",
+                    "TelescopePromptNormal",
+                    "TelescopePromptBorder",
+                    "TelescopePromptTitle",
+                    "TelescopePreviewTitle",
+                    "TelescopeResultsTitle",
+                    "TelescopePreviewNormal",
+                    "TelescopePreviewBorder",
+                    "TelescopeResultsNormal",
+                    "TelescopeResultsBorder",
+                    "TelescopeMatching",
+                },
+                extra_groups = {},
+                exclude_groups = {},
+                on_clear = function() end,
+            }
+            vim.cmd "TransparentEnable"
+        end,
+    },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,109 +1,141 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = "VeryLazy",
-    build = function()
-        require("nvim-treesitter.install").update { with_sync = true }
-    end,
-    opts = {
-        ensure_installed = {
-            -- JavaScript
-            "javascript",
-            "jsdoc",
-            "typescript",
-            "tsx",
-            "astro",
-            "vue",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = "VeryLazy",
+        build = function()
+            require("nvim-treesitter.install").update { with_sync = true }
+        end,
+        opts = {
+            ensure_installed = {
+                -- JavaScript
+                "javascript",
+                "jsdoc",
+                "typescript",
+                "tsx",
+                "astro",
+                "vue",
 
-            -- PHP
-            "php",
-            "php_only",
-            "twig",
+                -- PHP
+                "php",
+                "php_only",
+                "twig",
 
-            -- Python
-            "python",
+                -- Python
+                "python",
 
-            -- Go
-            "go",
-            "gomod",
-            "gowork",
-            "gosum",
-            "templ",
+                -- Go
+                "go",
+                "gomod",
+                "gowork",
+                "gosum",
+                "templ",
 
-            -- Rust
-            "rust",
+                -- Rust
+                "rust",
 
-            -- C/C++
-            "c",
-            "cpp",
+                -- C/C++
+                "c",
+                "cpp",
 
-            -- Others
-            "lua",
-            "luadoc",
-            "luap",
-            "html",
-            "xml",
-            "markdown",
-            "markdown_inline",
-            "css",
-            "json",
-            "jsonc",
-            "yaml",
-            "toml",
-            "vimdoc",
-            "vim",
-            "bash",
-            "dockerfile",
-            "diff",
-            "git_config",
-            "gitcommit",
-            "git_rebase",
-            "gitignore",
-            "gitattributes",
+                -- Others
+                "lua",
+                "luadoc",
+                "luap",
+                "html",
+                "xml",
+                "markdown",
+                "markdown_inline",
+                "css",
+                "json",
+                "jsonc",
+                "yaml",
+                "toml",
+                "vimdoc",
+                "vim",
+                "bash",
+                "dockerfile",
+                "diff",
+                "git_config",
+                "gitcommit",
+                "git_rebase",
+                "gitignore",
+                "gitattributes",
 
-            -- "ruby",
-            -- "elixir",
-            -- "heex",
-            -- "eex",
-            -- "zig",
+                -- "ruby",
+                -- "elixir",
+                -- "heex",
+                -- "eex",
+                -- "zig",
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true, disable = { "yaml" } },
+            rainbow = { enable = true },
         },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true, disable = { "yaml" } },
-        rainbow = { enable = true },
+        config = function(_, opts)
+            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+            parser_config.blade = {
+                install_info = {
+                    url = "https://github.com/EmranMR/tree-sitter-blade",
+                    files = { "src/parser.c" },
+                    branch = "main",
+                },
+                filetype = "blade",
+            }
+
+            parser_config.ejs = {
+                install_info = {
+                    url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+                    files = { "src/parser.c" },
+                    branch = "main",
+                },
+                filetype = "ejs",
+            }
+
+            vim.filetype.add {
+                pattern = {
+                    [".*%.blade%.php"] = "blade",
+                },
+            }
+
+            vim.filetype.add {
+                pattern = {
+                    [".*%.ejs"] = "blade",
+                },
+            }
+
+            require("nvim-treesitter.configs").setup(opts)
+        end,
     },
-    config = function(_, opts)
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-        parser_config.blade = {
-            install_info = {
-                url = "https://github.com/EmranMR/tree-sitter-blade",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "blade",
-        }
-
-        parser_config.ejs = {
-            install_info = {
-                url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "ejs",
-        }
-
-        vim.filetype.add {
-            pattern = {
-                [".*%.blade%.php"] = "blade",
-            },
-        }
-
-        vim.filetype.add {
-            pattern = {
-                [".*%.ejs"] = "blade",
-            },
-        }
-
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+    {
+        "windwp/nvim-ts-autotag",
+        event = "VeryLazy",
+        ft = {
+            "html",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "svelte",
+            "vue",
+            "tsx",
+            "jsx",
+            "rescript",
+            "xml",
+            "php",
+            "markdown",
+            "astro",
+            "glimmer",
+            "handlebars",
+            "hbs",
+        },
+        opts = {},
+    },
+    {
+        "folke/ts-comments.nvim",
+        opts = {},
+        event = "VeryLazy",
+        enabled = vim.fn.has "nvim-0.10.0" == 1,
+    },
 }
