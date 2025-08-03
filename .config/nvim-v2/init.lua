fe20e387afa7e73a0cf5f8f3b9dc797cd1c1b302
Shihab Mahamud <shihab4t@gmail.com>
2025-08-03 07:28:19 +0600

    otpions

--- .config/nvim-v2/init.lua
+++ .config/nvim-v2/init.lua
@@ -1,22 +1,22 @@
 -- General Editor Behavior
-vim.opt.swapfile = false         -- Disable swap file creation to reduce clutter
-vim.opt.undofile = true          -- Enable persistent undo history across sessions
-vim.opt.wrap = false             -- Disable line wrapping (long lines scroll horizontally)
-vim.opt.signcolumn = "yes"       -- Always show the sign column (avoids text shifting)
+vim.o.swapfile = false         -- Disable swap file creation to reduce clutter
+vim.o.undofile = true          -- Enable persistent undo history across sessions
+vim.o.wrap = false             -- Disable line wrapping (long lines scroll horizontally)
+vim.o.signcolumn = "yes"       -- Always show the sign column (avoids text shifting)
 
 -- Cursor and Line Display
-vim.opt.cursorline = true        -- Highlight the current line for visibility
-vim.opt.number = true            -- Show absolute line number on the current line
-vim.opt.relativenumber = true    -- Show relative numbers on other lines (useful for motions)
+vim.o.cursorline = true        -- Highlight the current line for visibility
+vim.o.number = true            -- Show absolute line number on the current line
+vim.o.relativenumber = true    -- Show relative numbers on other lines (useful for motions)
 
 -- Indentation and Tabs
 -- =========================================
-vim.opt.tabstop = 4              -- Number of spaces a <Tab> character displays as
-vim.opt.shiftwidth = 4           -- Number of spaces used for each indent level
+vim.o.tabstop = 4              -- Number of spaces a <Tab> character displays as
+vim.o.shiftwidth = 4           -- Number of spaces used for each indent level
 
 -- Invisible Characters Display
 -- =========================================
-vim.opt.list = true              -- Display invisible characters (tabs, spaces, etc.)
+vim.o.list = true              -- Display invisible characters (tabs, spaces, etc.)
 -- Customize how invisible characters are shown in the editor
 vim.opt.listchars = {
   space = "·",     -- Space characters
@@ -31,6 +31,6 @@ vim.opt.listchars = {
 
 -- Clipboard Integration
 vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"   -- Use system clipboard for all yank/paste actions
+  vim.o.clipboard = "unnamedplus"   -- Use system clipboard for all yank/paste actions
 end)
 
