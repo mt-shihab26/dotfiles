0c37fc5806214cf62d478b4996c33c249d3efbf0
Shihab Mahamud <shihab4t@gmail.com>
2025-08-05 11:45:15 +0600

    dev

--- .config/nvim/lua/plugins/assistant.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "A7lavinraj/assistant.nvim",
-    branch = "stable",
-    lazy = false,
-    keys = {
-        { "<leader>cp", "<cmd>Assistant<cr>", desc = "Assistant.nvim" },
-    },
-    opts = {},
-}
---
+++ .config/nvim/lua/plugins/dev.lua
@@ -0,0 +1,5 @@
+return {
+    {
+        dir = "~/projects/competitive-companion.nvim",
+    },
+}
