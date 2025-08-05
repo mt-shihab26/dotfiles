840304ec18036e518dbeb731082d470770f8030d
Shihab Mahamud <shihab4t@gmail.com>
2025-08-05 12:53:57 +0600

    nvim

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,8 +1,6 @@
 return {
     "akinsho/bufferline.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
     keys = {
         { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "prev buffer (bufferline)" },
