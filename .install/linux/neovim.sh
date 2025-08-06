083c291d331da8045df5b5dba26c44aa53860bde
Shihab Mahamud <shihab4t@gmail.com>
2025-08-07 02:34:09 +0600

    wip

--- .install/linux/neovim.sh
+++ .install/linux/neovim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S bob lazygit wl-clipboard lua luarocks imagemagick nodejs npm pnpm ripgrep fd base-devel gcc clang
+sudo pacman -S bob lazygit wl-clipboard lua luarocks imagemagick nodejs npm pnpm ripgrep fd base-devel gcc clang unzip
 
 bob install stable
 bob install nightly
