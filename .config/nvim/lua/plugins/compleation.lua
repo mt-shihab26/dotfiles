0bd7b5293d94fd667cb42f1f5210bae6d08d5acd
Shihab Mahamud <shihab4t@gmail.com>
2025-08-07 06:03:21 +0600

    waybar config

--- .config/nvim/lua/plugins/compleation.lua
+++ .config/nvim/lua/plugins/compleation.lua
@@ -61,7 +61,7 @@ return {
                 },
                 mapping = {
                     ["<CR>"] = cmp.mapping.confirm { select = true },
-                    ["<A-Tab>"] = cmp.mapping(function(fallback)
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
                         elseif luasnip.locally_jumpable(1) then
@@ -72,15 +72,15 @@ return {
                             fallback()
                         end
                     end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
+                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    --     if cmp.visible() then
+                    --         cmp.select_prev_item()
+                    --     elseif luasnip.locally_jumpable(-1) then
+                    --         luasnip.jump(-1)
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
                 },
                 sources = {
                     { name = "nvim_lsp" },
