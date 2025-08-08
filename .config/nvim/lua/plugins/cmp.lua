00ffee3e7d54e4e4faa7b568ba277215815969b5
Shihab Mahamud <shihab4t@gmail.com>
2025-08-09 05:26:32 +0600

    tabs

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -45,7 +45,7 @@ return {
             },
             mapping = {
                 ["<CR>"] = cmp.mapping.confirm { select = true },
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.locally_jumpable(1) then
@@ -56,15 +56,15 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-                -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-                --     if cmp.visible() then
-                --         cmp.select_prev_item()
-                --     elseif luasnip.locally_jumpable(-1) then
-                --         luasnip.jump(-1)
-                --     else
-                --         fallback()
-                --     end
-                -- end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             },
             sources = {
                 { name = "nvim_lsp" },
