b09bf4bbf577d22d2abec6684e84dfc313643ad6
Shihab Mahamud <shihab4t@gmail.com>
2025-08-10 00:15:31 +0600

    remove git

--- .config/nvim/lua/settings/intelephense.lua
+++ .config/nvim/lua/settings/intelephense.lua
@@ -1,16 +1,11 @@
 return {
-    -- Priority order: wp-config.php > .git > composer.json > index.php > file dir
+    -- Priority order: wp-config.php > .composer.json > index.php > file dir
     root_dir = function(fname)
         local lspconfig = require "lspconfig"
 
         local wp_root = lspconfig.util.root_pattern "wp-config.php"(fname)
         if wp_root then
             return wp_root
-        end
-
-        local git_root = lspconfig.util.root_pattern ".git"(fname)
-        if git_root then
-            return git_root
         end
 
         local composer_root = lspconfig.util.root_pattern "composer.json"(fname)
