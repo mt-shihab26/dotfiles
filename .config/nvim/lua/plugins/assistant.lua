e41c61aa64352823415e1bed53ef27078caa7133
Shihab Mahamud <shihab4t@gmail.com>
2025-08-10 09:08:10 +0600

    wip

---
+++ .config/nvim/lua/plugins/assistant.lua
@@ -0,0 +1,9 @@
+return {
+    "A7lavinraj/assistant.nvim",
+    branch = "stable",
+    lazy = false,
+    keys = {
+        { "<leader>ca", "<cmd>Assistant<cr>", desc = "Assistant.nvim" },
+    },
+    opts = {},
+}
