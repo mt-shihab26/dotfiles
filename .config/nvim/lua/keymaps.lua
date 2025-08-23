f42d0007c07bdf5c212668eadf7b9db0e7578013
Shihab Mahamud <shihab4t@gmail.com>
2025-08-23 12:58:46 +0600

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -14,3 +14,12 @@ vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "move to right window", nore
 vim.keymap.set("n", "<leader>ti", "<cmd>ToggleIndenChar<cr>", { desc = "toggle tabs/spaces for indentation" })
 vim.keymap.set("n", "<leader>tw", "<cmd>ToggleWrap<cr>", { desc = "toggle line wrapping on/off" })
 vim.keymap.set("n", "<leader>tc", "<cmd>ToggleCopilot<cr>", { desc = "toggle gitHub copilot on/off" })
+
+-- copy current file relative path
+vim.api.nvim_create_user_command("CopyFilePath", function()
+	local relative_path = vim.fn.expand("%")
+	vim.fn.setreg("+", relative_path)
+	print("Copied to clipboard: " .. relative_path)
+end, { desc = "Copy current file relative path to clipboard" })
+
+vim.keymap.set("n", "<leader>cp", "<cmd>CopyFilePath<cr>", { desc = "copy current file relative path" })
