055a377fd0de1e43af9b6704b8e23d9e791f9fdf
Shihab Mahamud <shihab4t@gmail.com>
2025-08-25 04:59:15 +0600

    copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
-    "zbirenbaum/copilot.lua",
-    enabled = false,
-    config = function()
-        require("copilot").setup {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-        }
-        -- vim.cmd "Copilot disable"
-    end,
+    -- "zbirenbaum/copilot.lua",
+    -- -- enabled = false,
+    -- config = function()
+    --     require("copilot").setup {
+    --         suggestion = { enabled = false },
+    --         panel = { enabled = false },
+    --     }
+    --     -- vim.cmd "Copilot disable"
+    -- end,
 }
