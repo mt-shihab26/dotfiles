887ff90feec7ee2147d14d5c21676afc55957c63
Shihab Mahamud <shihab4t@gmail.com>
2025-10-20 06:13:08 +0600

    wip

--- .scripts/hypr/tab-mode.sh
+++ .scripts/hypr/tab-mode.sh
@@ -1,42 +1,77 @@
 #!/usr/bin/env zsh
 
-# Toggle tab mode for all windows in the current workspace
-# Works with Hyprland
+# Map this script to a key to toggle "tab mode" (ON and OFF), where all windows
+# are placed inside one same (tabbed) group
 
-## Collect workspace/window info ###############################################
+## Collect some data about the environment #####################################
+
 ACTIVEWINDOW_JSON=$(hyprctl activewindow -j)
 CLIENTS_JSON=$(hyprctl clients -j)
 
 ORIGINAL_WORKSPACE=$(echo $ACTIVEWINDOW_JSON | jq ".workspace.id")
-ORIGINAL_WINDOW=$(echo $ACTIVEWINDOW_JSON | jq -r ".address")
+ORIGINAL_WINDOW=$(echo $ACTIVEWINDOW_JSON | jq ".address" | sed 's:"::g')
 
 WINDOWS_IN_GROUPS=$(echo $CLIENTS_JSON |
-    jq -r ".[] | select(.workspace.id == $ORIGINAL_WORKSPACE) | select(.grouped != []) | .address")
+    jq ".[] | select(.workspace.id == $ORIGINAL_WORKSPACE) | 
+                  select(.grouped != []) | .address" |
+    sed 's:"::g')
 
 WINDOWS_NOT_IN_GROUPS=$(echo $CLIENTS_JSON |
-    jq -r ".[] | select(.workspace.id == $ORIGINAL_WORKSPACE) | select(.grouped == []) | .address")
+    jq ".[] | select(.workspace.id == $ORIGINAL_WORKSPACE) | 
+                  select(.grouped == []) | .address" |
+    sed 's:"::g')
 
-## Remove all tab groups (toggle OFF) ##########################################
-if [ -n "$WINDOWS_IN_GROUPS" ]; then
-    COMMAND=""
-    for w in $WINDOWS_IN_GROUPS; do
-        COMMAND="$COMMAND dispatch focuswindow address:$w; dispatch moveoutofgroup; "
+## Remove all tab groups #######################################################
+
+COMMAND=""
+for w in $WINDOWS_IN_GROUPS; do
+    COMMAND="$COMMAND dispatch focuswindow address:$w; "
+    COMMAND="$COMMAND dispatch moveoutofgroup; "
+done
+! [ -z "$COMMAND" ] && hyprctl --batch "$COMMAND" >/dev/null
+
+## If toggling ON, group all windows under one single group ####################
+
+if ! [ -z "$WINDOWS_NOT_IN_GROUPS" ]; then
+
+    # Toggle group mode in the original window
+    #
+    COMMAND="         dispatch focuswindow address:$ORIGINAL_WINDOW; "
+    COMMAND="$COMMAND dispatch togglegroup" >/dev/null
+
+    hyprctl --batch $COMMAND >/dev/null
+
+    # Keep "merging" all other windows in all directions
+    #
+    for combo in u:d d:u l:r r:l; do
+
+        direction=$(echo $combo | cut -d: -f1)
+        opposite=$(echo $combo | cut -d: -f2)
+
+        counter=0
+        while true; do
+            hyprctl dispatch movefocus $direction >/dev/null
+
+            CURRENT_WINDOW=$(hyprctl activewindow -j |
+                jq ".address" | sed 's:"::g')
+
+            if [ $ORIGINAL_WINDOW == $CURRENT_WINDOW ]; then
+                break
+            fi
+
+            COMMAND="         dispatch moveintogroup $opposite; "
+            COMMAND="$COMMAND dispatch changegroupactive 1"
+
+            hyprctl --batch $COMMAND >/dev/null
+
+            # Security check, in case something goes terribly wrong
+            #
+            counter=$((counter + 1))
+            [ $counter -gt 10 ] && break
+        done
     done
-    hyprctl --batch "$COMMAND" >/dev/null
-    exit 0
 fi
 
-## Toggle ON: group all windows into one tab group #############################
-if [ -n "$WINDOWS_NOT_IN_GROUPS" ]; then
-    # Start group mode on the original window
-    hyprctl --batch "dispatch focuswindow address:$ORIGINAL_WINDOW; dispatch togglegroup" >/dev/null
+## Move focus back to the original window ######################33##############
 
-    # Merge all other ungrouped windows into the group
-    for w in $WINDOWS_NOT_IN_GROUPS; do
-        [ "$w" = "$ORIGINAL_WINDOW" ] && continue
-        hyprctl --batch "dispatch focuswindow address:$w; dispatch moveintogroup l" >/dev/null
-    done
-fi
-
-## Return focus to the original window #########################################
 hyprctl dispatch focuswindow address:$ORIGINAL_WINDOW >/dev/null
