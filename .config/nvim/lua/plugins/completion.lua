38fa91853f959e7366d65fce69c6d9294c8a3301
Shihab Mahamud <shihab4t@gmail.com>
2026-01-17 01:02:56 +0600

    completaion keymaps changed

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -18,13 +18,13 @@ return {
         local cmp = require "cmp"
         local luasnip = require "luasnip"
 
-        local has_words_before = function()
-            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-                return false
-            end
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
-        end
+        -- local has_words_before = function()
+        --     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        --         return false
+        --     end
+        --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        --     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match "^%s*$" == nil
+        -- end
 
         cmp.setup {
             preselect = cmp.PreselectMode.Item,
@@ -44,28 +44,37 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
-                        luasnip.jump(1)
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<CR>"] = cmp.mapping.confirm { select = true },
-                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+                ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+                ["<C-y>"] = cmp.mapping(
+                    cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    },
+                    { "i", "c" }
+                ),
+                -- ["<Tab>"] = cmp.mapping(function(fallback)
+                --     if cmp.visible() then
+                --         cmp.select_next_item()
+                --     elseif luasnip.locally_jumpable(1) then
+                --         luasnip.jump(1)
+                --     elseif has_words_before() then
+                --         cmp.complete()
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
+                -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+                --     if cmp.visible() then
+                --         cmp.select_prev_item()
+                --     elseif luasnip.locally_jumpable(-1) then
+                --         luasnip.jump(-1)
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
+                -- ["<CR>"] = cmp.mapping.confirm { select = true },
+                -- ["<C-Space>"] = cmp.mapping.complete(),
             },
             sources = {
                 { name = "nvim_lsp" },
