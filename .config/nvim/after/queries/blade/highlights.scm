44817f93f91225bf0555f618dc4c2eb3368712fa
Shihab Mahamud <shihab4t@gmail.com>
2026-01-19 00:56:10 +0600

    fixing queries

--- .config/nvim/after/queries/blade/highlights.scm
+++ .config/nvim/after/queries/blade/highlights.scm
@@ -1,12 +1,10 @@
 ; extends
 
-(directive) @function
-(directive_start) @function
-(directive_end) @function
+(directive) @keyword
+(directive_start) @keyword
+(directive_end) @keyword
 (comment) @comment
-((parameter) @include (#set! "priority" 110))
-((php_only) @include (#set! "priority" 110))
-((bracket_start) @function (#set! "priority" 120))
-((bracket_end) @function (#set! "priority" 120))
-(keyword) @function
+((bracket_start) @punctuation.special (#set! "priority" 120))
+((bracket_end) @punctuation.special (#set! "priority" 120))
+(keyword) @keyword
 
--- .config/nvim/after/queries/html/injections.scm
+++ .config/nvim/after/queries/html/injections.scm
@@ -1,4 +1,4 @@
-;; extends
+; extends
 
 ; AlpineJS attributes (x-*)
 (attribute
