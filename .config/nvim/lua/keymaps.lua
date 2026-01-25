ccefb391f7b9ec4eb196892f41377625973b13b0
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 23:36:42 +0600

    wip

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,13 +1,13 @@
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting", noremap = true })
-vim.keymap.set("n", "q", "<nop>", { desc = "disabled macro recording", noremap = true })
-vim.keymap.set("x", "p", '"_dP', { desc = "paste without yanking selected text", noremap = true })
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting" })
+vim.keymap.set("n", "q", "<nop>", { desc = "disable macro recording" })
+vim.keymap.set("x", "p", '"_dP', { desc = "paste without yanking selected text" })
 
-vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "move to left window", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "move to below window", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "move to above window", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "move to right window", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "move to left window" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "move to below window" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "move to above window" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "move to right window" })
 
-vim.keymap.set("n", "l", require "utils.open_last_file", { desc = "open last file or move right", noremap = true })
+vim.keymap.set("n", "l", require "utils.open_last_file", { desc = "open last file or move right" })
 vim.keymap.set("n", "<leader>ti", require "utils.toggle_indent_char", { desc = "toggle tabs/spaces for indentation" })
 vim.keymap.set("n", "<leader>tw", require "utils.toggle_wrap", { desc = "toggle line wrapping on/off" })
 
