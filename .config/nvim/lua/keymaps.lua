405acc9703c912f7ee72c51031d0757882ce90b1
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 23:03:47 +0600

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,41 +1,15 @@
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting" })
-
--- disable macro recording
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting", noremap = true })
 vim.keymap.set("n", "q", "<nop>", { desc = "disabled macro recording", noremap = true })
-
 vim.keymap.set("x", "p", '"_dP', { desc = "paste without yanking selected text", noremap = true })
 
-vim.keymap.set("n", "l", function()
-    local bufname = vim.api.nvim_buf_get_name(0)
-    local buftype = vim.api.nvim_buf_get_option(0, "buftype")
-    local modified = vim.api.nvim_buf_get_option(0, "modified")
-
-    if bufname == "" and buftype == "" and not modified then
-        local oldfiles = vim.v.oldfiles
-        if #oldfiles > 0 then
-            local lastfile = oldfiles[1]
-            lastfile = vim.fn.fnameescape(lastfile)
-            vim.cmd("edit " .. lastfile)
-        else
-            vim.notify("No recently opened files found", vim.log.levels.WARN)
-        end
-    else
-        vim.cmd "normal! l"
-    end
-end, { desc = "open last file or move right", noremap = true })
-
-vim.keymap.set("n", "<leader>x", "<cmd>CFExe<cr>", { desc = "execute current file based on language of cp" })
-
--- moving around nvim windows
 vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "move to left window", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "move to below window", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "move to above window", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "move to right window", noremap = true, silent = true })
 
--- toggle things
+vim.keymap.set("n", "l", require "lib.open_last_file", { desc = "open last file or move right", noremap = true })
 vim.keymap.set("n", "<leader>ti", "<cmd>ToggleIndenChar<cr>", { desc = "toggle tabs/spaces for indentation" })
 vim.keymap.set("n", "<leader>tw", "<cmd>ToggleWrap<cr>", { desc = "toggle line wrapping on/off" })
-vim.keymap.set("n", "<leader>tc", "<cmd>ToggleCopilot<cr>", { desc = "toggle gitHub copilot on/off" })
 
 -- copy current file relative path from file's directory
 vim.api.nvim_create_user_command("CopyFilePath", function()
