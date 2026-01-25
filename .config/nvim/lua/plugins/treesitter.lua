d24ff44fccd3a9aaf9fc0d3a4bdc09485f6f9a2b
Shihab Mahamud <shihab4t@gmail.com>
2026-01-25 23:16:23 +0600

    teesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -43,24 +43,7 @@ return {
         },
     },
     {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+        "folke/ts-comments.nvim",
         event = "VeryLazy",
-        opts = {
-            enable_autocmd = false,
-            languages = {
-                blade = "{{-- %s --}}",
-            },
-        },
-        config = function(_, opts)
-            require("ts_context_commentstring").setup(opts)
-
-            -- Integrate with native Neovim 0.10+ commenting
-            local get_option = vim.filetype.get_option
-            vim.filetype.get_option = function(filetype, option)
-                return option == "commentstring"
-                        and require("ts_context_commentstring.internal").calculate_commentstring()
-                    or get_option(filetype, option)
-            end
-        end,
     },
 }
