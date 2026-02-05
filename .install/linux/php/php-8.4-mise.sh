4470c786b31ff6d04b9bd7211aaea7d32993ab15
Shihab Mahamud <shihab4t@gmail.com>
2026-02-05 15:53:34 +0600

    wip

--- .install/linux/php/php-8.4-mise.sh
+++ .install/linux/php/php-8.4-mise.sh
@@ -1,3 +1,6 @@
+echo "Install dependencies for building php8.4 with mise"
+sudo pacman -Syu re2c autoconf automake libtool bison pkgconf openssl libxml2 sqlite curl oniguruma libzip icu
+
 echo "Build and install php8.4 with mise"
 mise install php@8.4
 
@@ -7,3 +10,8 @@ mise use -g php@8.4
 echo "Build and install extension with pecl"
 pecl install igbinary
 pecl install redis
+
+echo "Please add the following settings to your php.ini file:"
+echo "------------------------------------"
+echo "extension=igbinary"
+echo "extension=redis"
