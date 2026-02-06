dcdef06bdec9d3ceede3f2e86c77948f6dc16c38
Shihab Mahamud <shihab4t@gmail.com>
2026-02-07 03:20:05 +0600

    php 8.4 with mise

--- .install/linux/php/01_php-8.4-mise.sh
+++ .install/linux/php/01_php-8.4-mise.sh
@@ -4,7 +4,7 @@ echo "Install dependencies for building php8.4 with mise"
 sudo pacman -S re2c gd
 
 echo "Build and install php8.4 with mise"
-mise install php@8.4
+CONFIGURE_OPTS="--with-gmp" mise install php
 
 echo "Use php8.4 globally"
 mise use -g php@8.4
@@ -13,9 +13,9 @@ echo "Build and install extension with pecl"
 pecl install igbinary
 pecl install redis
 
-echo "Create a real PHP binary symlink in /usr/local/bin"
-sudo ln -sf ~/.local/share/mise/shims/php /usr/local/bin/php
-sudo ln -sf ~/.local/share/mise/shims/php-fpm /usr/local/bin/php-fpm
+# echo "Create a real PHP binary symlink in /usr/local/bin"
+# sudo ln -sf ~/.local/share/mise/shims/php /usr/local/bin/php
+# sudo ln -sf ~/.local/share/mise/shims/php-fpm /usr/local/bin/php-fpm
 
 echo "PHP configuration paths:"
 php --ini
@@ -26,3 +26,5 @@ echo "------------------------------------"
 echo "extension=igbinary"
 echo "extension=redis"
 echo "------------------------------------"
+
+mise unuse -g php@8.4
