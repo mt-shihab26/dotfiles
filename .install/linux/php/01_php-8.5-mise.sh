b00a462f4517ac6210ea403db2e9b5000ed4ba4e
Shihab Mahamud <shihab4t@gmail.com>
2026-02-06 17:46:17 +0600

    php 8.5

---
+++ .install/linux/php/01_php-8.5-mise.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Install dependencies for building php8.5 with mise"
+sudo pacman -Syu re2c gd
+
+echo "Build and install php8.5 with mise"
+mise install php@8.5
+
+echo "Use php8.5 globally"
+mise use -g php@8.5
+
+echo "Build and install extension with pecl"
+pecl install igbinary
+pecl install redis
+
+echo "Create a real PHP binary symlink in /usr/local/bin"
+sudo ln -sf ~/.local/share/mise/shims/php /usr/local/bin/php
+sudo ln -sf ~/.local/share/mise/shims/php-fpm /usr/local/bin/php-fpm
+
+echo "PHP configuration paths:"
+php --ini
+
+echo "------------------------------------"
+echo "Please add the following settings to your php.ini file:"
+echo "------------------------------------"
+echo "extension=igbinary"
+echo "extension=redis"
+echo "------------------------------------"
