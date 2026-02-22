cfda6aff6650e3b09f8dff69af9bd9e7ffac20ac
Shihab Mahamud <shihab4t@gmail.com>
2026-02-22 23:19:48 +0600

    wip

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -3,12 +3,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
-
--- Blade files: set filetype to php (for LSP) but use blade treesitter parser (for highlighting)
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.blade.php",
-    callback = function()
-        vim.bo.filetype = "php"
-        vim.treesitter.start(nil, "blade")
-    end,
-})
