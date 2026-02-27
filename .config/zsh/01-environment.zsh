a194941717b0407986f727d73eeab4806337edae
Shihab Mahamud <shihab4t@gmail.com>
2026-02-27 09:35:36 +0600

    wip

--- .config/zsh/01-environment.zsh
+++ .config/zsh/01-environment.zsh
@@ -1,11 +1,11 @@
 # Environment Variables
+export BAT_THEME=ansi
+export SUDO_EDITOR="$EDITOR"
 export OMARCHY_HOME="$HOME/.local/share/omarchy"
-export GO_HOME="$HOME/go"
 export COMPOSER_HOME="$HOME/.config/composer"
+export GO_HOME="$HOME/go"
 export JAVA_HOME="/usr/lib/jvm/default"
 export ANDROID_HOME="$HOME/Android/Sdk"
-export SUDO_EDITOR="$EDITOR"
-export BAT_THEME=ansi
 
 # PATH Management
 path_add() {
@@ -14,12 +14,11 @@ path_add() {
     fi
 }
 
-path_add "$HOME/.local/bin"
-path_add "$HOME/.local/share/nvim/mason/bin"
-path_add "$HOME/.local/share/bob/nvim-bin"
 path_add "$HOME/.local/share/omarchy/bin"
-path_add "$HOME/.cache/.bun/bin"
+path_add "$HOME/.local/share/nvim/mason/bin"
+path_add "$HOME/.local/bin"
 path_add "$COMPOSER_HOME/vendor/bin"
+path_add "$HOME/.cache/.bun/bin"
 path_add "$GO_HOME/bin"
 path_add "$JAVA_HOME/bin"
 path_add "$ANDROID_HOME/platform-tools"
