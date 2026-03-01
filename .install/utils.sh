d3ec9795bd18dbbf619e8d3c65d86f51909a6b75
Shihab Mahamud <shihab4t@gmail.com>
2026-03-01 07:40:51 +0600

    wip

--- .install/utils.sh
+++ .install/utils.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S htop stow cloc tree wget
+sudo pacman -S htop stow cloc tree wget gedit
+
+# Set gedit as default application for markdown files
+xdg-mime default gedit.desktop text/markdown
+xdg-mime default gedit.desktop text/x-markdown
