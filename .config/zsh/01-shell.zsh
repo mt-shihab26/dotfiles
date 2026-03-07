4f597b144c9c1199b0f30571412e84e8ad2fbd7e
Shihab Mahamud <shihab4t@gmail.com>
2026-03-07 22:37:40 +0600

    shell

--- .config/zsh/01-shell.zsh
+++ .config/zsh/01-shell.zsh
@@ -31,3 +31,33 @@ zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 
+# Prompt & Theme Setup
+# Powerlevel10k theme configuration
+
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet 
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+
+# Plugin Manager: Zinit
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+if [ ! -d "$ZINIT_HOME" ]; then
+   mkdir -p "$(dirname $ZINIT_HOME)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source "${ZINIT_HOME}/zinit.zsh"
+
+autoload -Uz compinit && compinit
+zinit cdreplay -q
+
+# Load Plugins with Zinit
+zinit ice depth=1
+zinit light romkatv/powerlevel10k
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+zinit snippet OMZP::git
+zinit snippet OMZP::sudo
+zinit snippet OMZP::command-not-found
+
--- .config/zsh/40-theme.zsh
+++
@@ -1,5 +0,0 @@
-# Prompt & Theme Setup
-# Powerlevel10k theme configuration
-
-typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet 
-[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
