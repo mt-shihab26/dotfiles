5e1b43bd540ac852214d77124375c67cb399b4f5
Shihab Mahamud <shihab4t@gmail.com>
2026-03-07 22:36:10 +0600

    shell

--- .config/zsh/01-shell.zsh
+++ .config/zsh/01-shell.zsh
@@ -2,7 +2,7 @@
 # Set interactive shell check
 [[ $- != *i* ]] && return
 
-# History Settings
+# History control
 HISTSIZE=5000
 HISTFILE=~/.zsh_history
 SAVEHIST=$HISTSIZE
@@ -20,3 +20,14 @@ bindkey -e
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
 bindkey '^[w' kill-region
+
+# Completion Settings
+autoload -Uz compinit && compinit
+
+# Zsh completion configuration
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
