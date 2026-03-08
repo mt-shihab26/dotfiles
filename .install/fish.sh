c2c464b15c3da3a9aae9e046f48a36bd091385f9
Shihab Mahamud <shihab4t@gmail.com>
2026-03-08 07:55:14 +0600

    fish

---
+++ .install/fish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+sudo pacman -S fish
+
+chsh -s "$(which fish)"
+
+echo -e "\nInstallation complete. Please restart your terminal or log out and back in to start using Fish."
