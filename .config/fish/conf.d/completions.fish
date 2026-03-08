a57aedec084c2c15523cb8a5a322f8a93e605da6
Shihab Mahamud <shihab4t@gmail.com>
2026-03-08 23:37:14 +0600

    remove fish use bash

--- .config/fish/conf.d/completions.fish
+++
@@ -1,9 +0,0 @@
-# Bun completions
-if command -v bun &>/dev/null
-    bun completions | source
-end
-
-# Rustup completions
-if command -v rustup &>/dev/null
-    rustup completions fish rustup | source
-end
