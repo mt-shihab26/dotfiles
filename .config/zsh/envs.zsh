9db1a670f5fde0c1b0aac8efb7e110d5bd4b9b6c
Shihab Mahamud <shihab4t@gmail.com>
2026-03-29 01:17:46 +0600

    zshrc

---
+++ .config/zsh/envs.zsh
@@ -0,0 +1,12 @@
+# Editor used by CLI
+export SUDO_EDITOR="$EDITOR"
+export BAT_THEME=ansi
+
+# Duplicated from .config/uwsm/env so SSH works too
+export OMARCHY_PATH=$HOME/.local/share/omarchy
+export PATH=$PATH:$OMARCHY_PATH/bin
+
+# General binaries
+export PATH=$PATH:$HOME/.bin
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.local/share/nvim/mason/bin
