9c33ce138746e2a0abdce5c59495eda6ed2ad42b
Shihab Mahamud <shihab4t@gmail.com>
2026-07-02 04:41:49 +0600

    wip

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,45 +1,50 @@
+-- netrw
 vim.g.netrw_banner = 0
 
+-- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
+-- line display
 vim.opt.wrap = false
-vim.opt.smartindent = true 
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = { space = "·", trail = "·", tab = "→ ", eol = "↲", nbsp = "␣" }
+vim.opt.signcolumn = "yes"
+vim.o.cmdheight = 0
 
-vim.opt.inccommand = "split" 
-
+-- splits
 vim.opt.splitright = true
-vim.opt.splitbelow = true 
+vim.opt.splitbelow = true
 
+-- search
 vim.opt.ignorecase = true
-vim.opt.smartcase = true 
-vim.opt.laststatus = 3 
+vim.opt.smartcase = true
+vim.opt.inccommand = "split"
 
+-- files & undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-
 vim.opt.isfname:append("@-@")
 
-vim.opt.cursorline = true
-
-vim.opt.signcolumn = "yes"
-vim.opt.list = true -- show invisible characters
-vim.opt.listchars = { space = "·", trail = "·", tab = "→ ", eol = "↲", nbsp = "␣" }
-
-vim.o.cmdheight = 0
-
-vim.opt.mouse = "a"
-
+-- spell
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
-vim.opt.termguicolors = true
 
+-- ui & input
+vim.opt.laststatus = 3
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+
+-- clipboard (deferred to avoid startup latency)
 vim.schedule(function()
     vim.opt.clipboard:append("unnamedplus")
 end)
