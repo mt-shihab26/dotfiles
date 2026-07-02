38399a335d617d0bb390807cd238f1a58cb07864
Shihab Mahamud <shihab4t@gmail.com>
2026-07-02 07:54:14 +0600

    wip

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -3,6 +3,14 @@ vim.pack.add({
     src = "https://github.com/folke/tokyonight.nvim",
     version = vim.version.range("4"),
   },
+  "https://github.com/NvChad/nvim-colorizer.lua",
+})
+
+require("colorizer").setup({
+  user_default_options = {
+    names = false,
+    tailwind = true,
+  },
 })
 
 require("tokyonight").setup({})
--- .config/nvim/memory.md
+++
@@ -1,1 +0,0 @@
-Never remove { confirm = false } from vim.pack.add calls - user always wants it
