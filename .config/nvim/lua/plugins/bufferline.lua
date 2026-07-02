c6d282ac95ed18fd88d3a67eb545c41263f1a493
Shihab Mahamud <shihab4t@gmail.com>
2026-07-02 06:09:32 +0600

    move close_terminals_or_others into utils

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -44,33 +44,5 @@ require("bufferline").setup({
   },
 })
 
-local function close_terminals_or_others()
-  local bufs = vim.api.nvim_list_bufs()
-  local current = vim.api.nvim_get_current_buf()
-  local terminals = {}
-  local others = {}
-
-  for _, buf in ipairs(bufs) do
-    if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then
-      if vim.bo[buf].buftype == "terminal" then
-        terminals[#terminals + 1] = buf
-      elseif vim.bo[buf].buflisted then
-        others[#others + 1] = buf
-      end
-    end
-  end
-
-  local targets = #terminals > 0 and terminals or others
-  if #targets == 0 then
-    vim.notify("No buffers to close", vim.log.levels.INFO)
-    return
-  end
-
-  for _, buf in ipairs(targets) do
-    vim.cmd("bdelete! " .. buf)
-  end
-end
-
-_G.close_terminals_or_others = close_terminals_or_others
 
 
---
+++ .config/nvim/lua/utils/close_terminals_or_others.lua
@@ -0,0 +1,26 @@
+return function()
+  local bufs = vim.api.nvim_list_bufs()
+  local current = vim.api.nvim_get_current_buf()
+  local terminals = {}
+  local others = {}
+
+  for _, buf in ipairs(bufs) do
+    if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then
+      if vim.bo[buf].buftype == "terminal" then
+        terminals[#terminals + 1] = buf
+      elseif vim.bo[buf].buflisted then
+        others[#others + 1] = buf
+      end
+    end
+  end
+
+  local targets = #terminals > 0 and terminals or others
+  if #targets == 0 then
+    vim.notify("No buffers to close", vim.log.levels.INFO)
+    return
+  end
+
+  for _, buf in ipairs(targets) do
+    vim.cmd("bdelete! " .. buf)
+  end
+end
