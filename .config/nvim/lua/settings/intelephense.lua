d2a54fc174ef35f4254c3a2eec7c892559c1090a
Shihab Mahamud <shihab4t@gmail.com>
2026-08-09 00:15:55 +0600

    fix intelephense LSP in blade PHP blocks by reporting languageId as php

--- .config/nvim/lua/settings/intelephense.lua
+++ .config/nvim/lua/settings/intelephense.lua
@@ -1,4 +1,10 @@
 return {
     filetypes = { "php", "blade" },
     root_markers = { "wp-config.php", "composer.json" },
+    get_language_id = function(_, ftype)
+        if ftype == "blade" then
+            return "php"
+        end
+        return ftype
+    end,
 }
