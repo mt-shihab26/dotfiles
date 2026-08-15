195a16a58b8b5efb6078fbdc8ab9c3004695e11b
Shihab Mahamud <shihab4t@gmail.com>
2026-08-16 03:56:09 +0600

    all agents

---
+++ .install/agents.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yay -Sy --noconfirm --needed claude-code
+sudo pacman -Sy --noconfirm openai-codex
+sudo pacman -Sy --noconfirm opencode
+yay -Sy --noconfirm --needed github-copilot-cli-bin
--- .install/git.sh
+++ .install/git.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
 git config --global init.defaultBranch main
+
+sudo pacman -S github-cli
