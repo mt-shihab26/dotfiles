de98ef73ace69110d9fe9b0ca789b850f31596a6
Shihab Mahamud <shihab4t@gmail.com>
2026-08-27 17:14:08 +0600

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,74 +1,76 @@
-vim.keymap.set("n", "q", "<nop>", { desc = "disable macro recording" })
+local map = vim.keymap.set
 
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting" })
+map("n", "q", "<nop>", { desc = "disable macro recording" })
 
-vim.keymap.set("x", "p", '"_dP', { desc = "paste without yanking selected text" })
+map("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "clear search highlighting" })
 
-vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "move to below window" })
-vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "move to above window" })
-vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "move to right window" })
+map("x", "p", '"_dP', { desc = "paste without yanking selected text" })
 
-vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "navigate left window (tmux)" })
-vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>", { desc = "navigate down window (tmux)" })
-vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>", { desc = "navigate up window (tmux)" })
-vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>", { desc = "navigate right window (tmux)" })
+map("n", "<leader>j", "<C-w>j", { desc = "move to below window" })
+map("n", "<leader>k", "<C-w>k", { desc = "move to above window" })
+map("n", "<leader>l", "<C-w>l", { desc = "move to right window" })
 
-vim.keymap.set("n", "l", require "utils.open_last_file", { desc = "open last file or move right" })
+map("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "navigate left window (tmux)" })
+map("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<cr>", { desc = "navigate down window (tmux)" })
+map("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<cr>", { desc = "navigate up window (tmux)" })
+map("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<cr>", { desc = "navigate right window (tmux)" })
 
-local toggle = require "utils.toggle"
+map("n", "l", require "utils.open_last_file", { desc = "open last file or move right" })
 
-vim.keymap.set("n", "<leader>ti", toggle.indent_char, { desc = "toggle tabs/spaces for indentation" })
-vim.keymap.set("n", "<leader>tw", toggle.wrap, { desc = "toggle line wrapping on/off" })
+local toggle = require "lib.toggle"
 
-local copy_file = require "utils.copy_file"
+map("n", "<leader>ti", toggle.indent_char, { desc = "toggle tabs/spaces for indentation" })
+map("n", "<leader>tw", toggle.wrap, { desc = "toggle line wrapping on/off" })
 
-vim.keymap.set("n", "<leader>cp", copy_file.path, { desc = "copy current file relative path" })
-vim.keymap.set("n", "<leader>ca", copy_file.abs_path, { desc = "copy current file absolute path" })
-vim.keymap.set("n", "<leader>cf", copy_file.name, { desc = "copy current file name with extension" })
+local copy_path = require "lib.copy_path"
 
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree action=focus<cr>", { desc = "focus file tree (neo-tree)", remap = true })
-vim.keymap.set("n", "<leader>p", "<cmd>Neotree action=close<cr>", { desc = "close file tree (neo-tree)", remap = true })
+map("n", "<leader>cp", copy_path.path, { desc = "copy current file relative path" })
+map("n", "<leader>ca", copy_path.abs_path, { desc = "copy current file absolute path" })
+map("n", "<leader>cf", copy_path.name, { desc = "copy current file name with extension" })
 
-vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "prev buffer (bufferline)" })
-vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "next buffer (bufferline)" })
-vim.keymap.set("n", "<leader>Q", "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>", { desc = "close current buffer (bufferline)" })
-vim.keymap.set(
+map("n", "<leader>e", "<cmd>Neotree action=focus<cr>", { desc = "focus file tree (neo-tree)", remap = true })
+map("n", "<leader>p", "<cmd>Neotree action=close<cr>", { desc = "close file tree (neo-tree)", remap = true })
+
+map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "prev buffer (bufferline)" })
+map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "next buffer (bufferline)" })
+map("n", "<leader>Q", "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>", { desc = "close current buffer (bufferline)" })
+map(
     "n",
     "<leader>q",
     require "utils.close_terminals_or_others",
     { desc = "close terminals or other buffers (bufferline)" }
 )
-vim.keymap.set(
+map(
     "n",
     "<leader>cl",
     "<cmd>BufferLineCloseRight<cr>",
     { desc = "delete buffers to the right (bufferline)" }
 )
-vim.keymap.set("n", "<leader>ch", "<cmd>BufferLineCloseLeft<cr>", { desc = "delete buffers to the left (bufferline)" })
+map("n", "<leader>ch", "<cmd>BufferLineCloseLeft<cr>", { desc = "delete buffers to the left (bufferline)" })
 
 local telescope = require "utils.telescope"
 
-vim.keymap.set("n", "<leader>f", telescope.find_files, { desc = "find files (telescope)" })
-vim.keymap.set("n", "<leader>F", telescope.find_all_files, { desc = "find all files (telescope)" })
-vim.keymap.set("n", "<leader>g", telescope.grep_files, { desc = "grep files (telescope)" })
-vim.keymap.set("n", "<leader>G", telescope.grep_all_files, { desc = "grep all files (telescope)" })
-vim.keymap.set("n", "<leader>h", telescope.help_tags, { desc = "help tags (telescope)" })
-vim.keymap.set("n", "<leader>H", telescope.lsp_document_symbols, { desc = "lsp document symbols (telescope)" })
+map("n", "<leader>f", telescope.find_files, { desc = "find files (telescope)" })
+map("n", "<leader>F", telescope.find_all_files, { desc = "find all files (telescope)" })
+map("n", "<leader>g", telescope.grep_files, { desc = "grep files (telescope)" })
+map("n", "<leader>G", telescope.grep_all_files, { desc = "grep all files (telescope)" })
+map("n", "<leader>h", telescope.help_tags, { desc = "help tags (telescope)" })
+map("n", "<leader>H", telescope.lsp_document_symbols, { desc = "lsp document symbols (telescope)" })
 
 
-vim.keymap.set("n", "<leader>?", function()
+map("n", "<leader>?", function()
     require("which-key").show { global = false }
 end, { desc = "buffer local keymaps (which-key)" })
 
-vim.keymap.set("n", "<leader>L", "<cmd>LazyGit<cr>", { desc = "open lazygit window (lazygit)", remap = true })
+map("n", "<leader>L", "<cmd>LazyGit<cr>", { desc = "open lazygit window (lazygit)", remap = true })
 
-vim.keymap.set("n", "<leader>s", function()
+map("n", "<leader>s", function()
     require("spectre").open_file_search { select_word = true }
 end, { desc = "search on current buffer (spectre)" })
-vim.keymap.set("n", "<leader>S", function()
+map("n", "<leader>S", function()
     require("spectre").open_visual { select_word = true }
 end, { desc = "search on multi buffers (spectre)" })
-vim.keymap.set("v", "<leader>S", function()
+map("v", "<leader>S", function()
     require("spectre").open_visual()
 end, { desc = "search on multi buffers (spectre)" })
 
@@ -109,8 +111,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
-vim.keymap.set("n", "]h", "<cmd>silent Gitsigns next_hunk<cr>", { desc = "navigate to the next git hunk (gitsigns)" })
-vim.keymap.set(
+map("n", "]h", "<cmd>silent Gitsigns next_hunk<cr>", { desc = "navigate to the next git hunk (gitsigns)" })
+map(
     "n",
     "[h",
     "<cmd>silent Gitsigns prev_hunk<cr>",
