079b351162c7ccc3bf1fe02ad00a9ec62d47a273
Shihab Mahamud <shihab4t@gmail.com>
2026-09-18 07:03:41 +0600

    top bar

--- .setup/top-bar.sh
+++ .setup/top-bar.sh
@@ -11,8 +11,8 @@ fi
 omarchy plugin enable omarchy.indicators --section center --index 0
 omarchy plugin enable omarchy-orivo-plugin --section center --after omarchy.indicators
 omarchy plugin enable omarchy.clock --section center --after omarchy-orivo-plugin
-omarchy plugin enable omarchy.weather --section center --after omarchy.clock 
-omarchy plugin enable omarchy.keyboard-layout --section center --after omarchy.weather
+omarchy plugin enable omarchy.keyboard-layout --section center --after omarchy.clock
+omarchy plugin enable omarchy.weather --section center --after omarchy.keyboard-layout
 omarchy plugin enable omarchy.microphone --section center --after omarchy.keyboard-layout
 omarchy plugin enable omarchy.system-update --section center --after omarchy.microphone
 
